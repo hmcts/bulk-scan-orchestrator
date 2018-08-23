03f1e07f26279577f73877f46ee75c7af856861e
leszekg <gonczar@gmail.com>
2018-08-23 17:47:09 +0100

    Add smoke and functional test modules with empty tests

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/ExampleTest.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void replace_this_with_actual_tests() throws Exception {
+        // TODO: this test is there so that a test report can be created for smoke tests
+        // (otherwise the build fails). Remove when actual smoke tests have been written.
+    }
+}
