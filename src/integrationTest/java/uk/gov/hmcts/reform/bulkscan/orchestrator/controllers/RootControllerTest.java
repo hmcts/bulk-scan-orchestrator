007d9664a6fcc3c7c4524582c0179666afff4834
leszekg <gonczar@gmail.com>
2018-08-23 15:06:44 +0100

    Add integrationTest directory with one test inside

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/RootControllerTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class RootControllerTest {
+
+    @Autowired
+    private transient MockMvc mockMvc;
+
+    @Test
+    public void call_to_root_endpoint_should_result_with_204_response() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isNoContent());
+    }
+}
