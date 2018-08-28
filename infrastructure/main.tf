b100d991b1b51e3ad9ff28b7f8582e7e460059d1
Leszek Gonczar <gonczar@gmail.com>
2018-08-29 00:36:41 +0100

    Add Service Bus queue definition to terraform (#7)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,26 @@ resource "azurerm_resource_group" "rg" {
 }
 
 locals {
-  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  is_preview             = "${(var.env == "preview" || var.env == "spreview")}"
+  previewVaultName       = "${var.product}-bsp"
+  nonPreviewVaultName    = "${var.product}-bsp-${var.env}"
+  vaultName              = "${local.is_preview ? local.previewVaultName : local.nonPreviewVaultName}"
+  keyVaultUri            = "https://rpe-bsp-${var.env}.vault.azure.net/"
+}
+
+module "queue-namespace" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace.git"
+  name                = "${var.product}-${var.component}-servicebus-${var.env}"
+  location            = "${var.location_app}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
+module "queue" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue.git"
+  name                = "envelopes"
+  namespace_name      = "${module.queue-namespace.name}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
 }
 
 module "bulk-scan-orchestrator" {
@@ -24,5 +43,13 @@ module "bulk-scan-orchestrator" {
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
     LOGBACK_REQUIRE_ERROR_CODE  = false
+    QUEUE_CONNECTION_STRING     = "${module.queue.primary_listen_connection_string}"
   }
+}
+
+resource "azurerm_key_vault_secret" "queue_send_connection_string" {
+  name      = "envelope-queue-send-conn-string"
+  value     = "${module.queue.primary_send_connection_string}"
+  vault_uri = "${local.keyVaultUri}"
+  count     = "${is_preview ? "0": "1"}"
 }
