1d473ec7ca9482be74dedef7e70bb17aa4a43279
Tim Jacomb <tim.jacomb@hmcts.net>
2018-09-05 15:52:57 +0100

    RPE-680 Shared infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,20 +15,6 @@ locals {
   keyVaultUri         = "https://rpe-bsp-${var.env}.vault.azure.net/"
 }
 
-module "queue-namespace" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace.git"
-  name                = "${var.product}-${var.component}-servicebus-${var.env}"
-  location            = "${var.location_app}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-}
-
-module "queue" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue.git"
-  name                = "envelopes"
-  namespace_name      = "${module.queue-namespace.name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-}
-
 module "bulk-scan-orchestrator" {
   source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product                         = "${var.product}-${var.component}"
@@ -46,13 +32,6 @@ module "bulk-scan-orchestrator" {
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
     LOGBACK_REQUIRE_ERROR_CODE  = false
-    QUEUE_CONNECTION_STRING     = "${module.queue.primary_listen_connection_string}"
+    QUEUE_CONNECTION_STRING     = "${data.terraform_remote_state.queue.primary_listen_connection_string}"
   }
-}
-
-resource "azurerm_key_vault_secret" "queue_send_connection_string" {
-  name      = "envelope-queue-send-conn-string"
-  value     = "${module.queue.primary_send_connection_string}"
-  vault_uri = "${local.keyVaultUri}"
-  count     = "${local.is_preview ? "0": "1"}"
 }
--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -23,3 +23,14 @@ data "terraform_remote_state" "core_apps_compute" {
     key                  = "core-compute/${var.env}/terraform.tfstate"
   }
 }
+
+data "terraform_remote_state" "queue" {
+  backend = "azurerm"
+
+  config {
+    resource_group_name  = "mgmt-state-store-${var.subscription}"
+    storage_account_name = "mgmtstatestore${var.subscription}"
+    container_name       = "mgmtstatestorecontainer${var.env}"
+    key                  = "bulk-scan/${var.env}/terraform.tfstate"
+  }
+}
