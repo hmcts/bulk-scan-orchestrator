10714460e4d64edbfd5a77427035bf3317b2835f
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-09-05 16:24:28 +0100

    Add bulk scan processor client (#12)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/bulkscanprocessorclient/client/BulkScanProcessorClient.java
@@ -0,0 +1,44 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.services.bulkscanprocessorclient.client;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.bulkscanprocessorclient.exceptions.ReadEnvelopeException;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.bulkscanprocessorclient.model.Envelope;
+
+import java.util.function.Supplier;
+
+@Service
+public class BulkScanProcessorClient {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String baseUrl;
+    private final Supplier<String> serviceAuthTokenSupplier;
+
+    public BulkScanProcessorClient(
+        String baseUrl,
+        Supplier<String> serviceAuthTokenSupplier
+    ) {
+        this.baseUrl = baseUrl;
+        this.serviceAuthTokenSupplier = serviceAuthTokenSupplier;
+    }
+
+    public Envelope getEnvelopeById(String id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("ServiceAuthorization", this.serviceAuthTokenSupplier.get());
+
+        try {
+            return restTemplate.exchange(
+                this.baseUrl + "/envelopes/" + id,
+                HttpMethod.GET,
+                new HttpEntity<String>(headers),
+                Envelope.class
+            ).getBody();
+        } catch (RestClientException exc) {
+            throw new ReadEnvelopeException(exc);
+        }
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/bulkscanprocessorclient/model/Envelope.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.services.bulkscanprocessorclient.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Envelope {
+
+    public final String id;
+    public final String zipFileName;
+
+    public Envelope(
+        @JsonProperty("id") String id,
+        @JsonProperty("zip_file_name") String zipFileName
+    ) {
+        this.id = id;
+        this.zipFileName = zipFileName;
+    }
+}
