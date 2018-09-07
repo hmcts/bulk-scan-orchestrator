8747e59046067b1f97d53225db8455d4bba7ec68
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-09-07 10:34:49 +0100

    Add s2s client (#16)

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AppConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AppConfiguration.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.bulkscanprocessorclient.client.BulkScanProcessorClient;
 
 @Configuration
@@ -10,11 +11,12 @@ public class AppConfiguration {
 
     @Bean
     public BulkScanProcessorClient envelopeService(
-        @Value("${bulk-scan-processor-url}") String bulkScanProcessorUrl
+        @Value("${bulk-scan-processor-url}") String bulkScanProcessorUrl,
+        AuthTokenGenerator authTokenGenerator
     ) {
         return new BulkScanProcessorClient(
             bulkScanProcessorUrl,
-            () -> "s2s_token" // TODO: use s2s client to get token
+            authTokenGenerator::generate
         );
     }
 }
