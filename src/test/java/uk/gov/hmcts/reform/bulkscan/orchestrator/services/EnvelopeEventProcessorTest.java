997d3b91fde6d5ea9e849f978e49d5a9b9227794
Rafal Kondratowicz <rkondr@gmail.com>
2018-09-19 16:21:06 +0100

    Add test

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/EnvelopeEventProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/EnvelopeEventProcessorTest.java
@@ -1,18 +1,36 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services;
 
+import com.microsoft.azure.servicebus.IMessage;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EnvelopeEventProcessorTest {
 
+    @Mock
+    private IMessage someMessage;
     private EnvelopeEventProcessor processor;
 
     @Before
     public void before() {
         processor = new EnvelopeEventProcessor();
+    }
+
+    @Test
+    public void should_return_completed_future_if_everything_went_fine() {
+        // when
+        CompletableFuture<Void> result = processor.onMessageAsync(someMessage);
+
+        // then
+        assertThat(result.isDone()).isTrue();
+        assertThat(result.isCompletedExceptionally()).isFalse();
     }
 
     @Test
