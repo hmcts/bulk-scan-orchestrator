5a81f78a287739967c64df92225d61596de69093
Tim Jacomb <tim.jacomb@hmcts.net>
2018-09-20 07:53:15 +0100

    Make it work better

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/idam/UserMapping.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "idam")
+public class UserMapping {
+    private Map<String, Credential> users;
+
+    public Map<String, Credential> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Map<String, Credential> users) {
+        this.users = users;
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/idam/UserService.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/idam/UserService.java
@@ -1,26 +1,27 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
-
-import java.util.Map;
 
 @Service
 public class UserService {
 
     private final IdamClient idamClient;
-    private final Map<String, Credential> users;
+    private final UserMapping users;
 
     @Autowired
-    public UserService(IdamClient idamClient, @Value("idam.users") Map<String, Credential> users) {
+    public UserService(IdamClient idamClient, UserMapping users) {
         this.idamClient = idamClient;
         this.users = users;
     }
 
     public String getBearerTokenForJurisdiction(String jurisdiction) {
-        Credential user = users.get(jurisdiction);
+        Credential user = users.getUsers().get(jurisdiction);
+        if (user == null) {
+            throw new NoUserConfiguredException(jurisdiction);
+        }
+
         return idamClient.authenticateUser(user.getUsername(), user.getPassword());
     }
 }
