eca52d5cc9f10dce94bf0c91be60f2f69f5b962d
Rafal Kondratowicz <rkondr@gmail.com>
2018-09-20 16:13:10 +0100

    Fix sample data

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/SampleData.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/SampleData.java
@@ -10,8 +10,8 @@ public class SampleData {
 
     public static String envelopeJson() throws Exception {
         return new JSONObject()
-            .put("id", "")
-            .put("case_ref", "eb9c3598-35fc-424e-b05a-902ee9f11d56")
+            .put("id", "eb9c3598-35fc-424e-b05a-902ee9f11d56")
+            .put("case_ref", "ABC123")
             .put("jurisdiction", "SSCS")
             .put("classification", Classification.NEW_APPLICATION)
             .put("doc_urls", new JSONArray(asList("a", "b")))
