89a32a2a67fb14366a07c4c8b26f4e3d571274c4
Kevlar <kevin.j.cross@gmail.com>
2018-09-26 14:10:55 +0100

    fixing checkstyle violation.

--- src/test/java/GetSasToken.java
+++ src/test/java/GetSasToken.java
@@ -5,7 +5,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-public class GetSasToken {
+public class GetSASToken {
 
     public static void main(String[] args) throws Exception {
 
