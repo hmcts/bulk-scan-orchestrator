1e36bac09f947545b42f191cbf0aab24de80d63a
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-10-02 15:25:41 +0100

    [PREVIEW] Use new document model (#56)

---
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/DatetimeHelper.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+public class DatetimeHelper {
+
+    public static String toIso8601(Instant instant) {
+        return ZonedDateTime
+            .ofInstant(instant, ZoneId.of("UTC"))
+            .format(ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+    }
+
+    private DatetimeHelper() {
+        // util class
+    }
+}
