6f8b81d18fa64ab806ac45bc79237bffc0826b64
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-10-08 14:36:49 +0100

    Load balance case data api access in retrieval service

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/EnvelopeEventProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/EnvelopeEventProcessor.java
@@ -6,6 +6,7 @@ import com.microsoft.azure.servicebus.IMessage;
 import com.microsoft.azure.servicebus.IMessageHandler;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CaseRetriever;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdAuthenticator;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.model.Envelope;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
@@ -43,10 +44,15 @@ public class EnvelopeEventProcessor implements IMessageHandler {
         Envelope envelope = parse(message.getBody());
         CaseDetails theCase = Strings.isNullOrEmpty(envelope.caseRef)
             ? null
-            : caseRetriever.retrieve(envelope.jurisdiction, envelope.caseRef);
+            : retrieveCase(envelope.jurisdiction, envelope.caseRef);
 
         // - create record from envelope and case
         // - supply it to ccd event publisher
+    }
+
+    private CaseDetails retrieveCase(String jurisdiction, String caseRef) {
+        CcdAuthenticator authenticator = caseRetriever.authenticate(jurisdiction);
+        return caseRetriever.retrieve(jurisdiction, caseRef, authenticator);
     }
 
     @Override
--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CaseRetriever.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CaseRetriever.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
@@ -13,6 +18,8 @@ public class CaseRetriever {
     private static final Logger log = LoggerFactory.getLogger(CaseRetriever.class);
 
     private final CcdAuthenticatorFactory factory;
+
+    @LoadBalanced
     private final CoreCaseDataApi coreCaseDataApi;
 
     CaseRetriever(CcdAuthenticatorFactory factory, CoreCaseDataApi coreCaseDataApi) {
@@ -20,9 +27,26 @@ public class CaseRetriever {
         this.coreCaseDataApi = coreCaseDataApi;
     }
 
-    public CaseDetails retrieve(String jurisdiction, String caseRef) {
-        CcdAuthenticator info = factory.createForJurisdiction(jurisdiction);
-        CaseDetails caseDetails = retrieveCase(jurisdiction, caseRef, info);
+    public CcdAuthenticator authenticate(String jurisdiction) {
+        return factory.createForJurisdiction(jurisdiction);
+    }
+
+    @HystrixCommand(
+        commandKey = "case-retrieval",
+        commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+        },
+        fallbackMethod = "retrieveFallback"
+    )
+    public CaseDetails retrieve(String jurisdiction, String caseRef, CcdAuthenticator authenticator) {
+        CaseDetails caseDetails = coreCaseDataApi.readForCaseWorker(
+            authenticator.getUserToken(),
+            authenticator.getServiceToken(),
+            authenticator.userDetails.getId(),
+            jurisdiction,
+            CASE_TYPE_ID,
+            caseRef
+        );
 
         if (caseDetails != null) {
             log.info(
@@ -36,15 +60,56 @@ public class CaseRetriever {
         return caseDetails;
     }
 
-    private CaseDetails retrieveCase(String jurisdiction, String caseRef, CcdAuthenticator authenticator) {
-        return coreCaseDataApi.readForCaseWorker(
-            authenticator.getUserToken(),
-            authenticator.getServiceToken(),
-            authenticator.userDetails.getId(),
-            jurisdiction,
-            CASE_TYPE_ID,
-            caseRef
-        );
+    @SuppressWarnings("unused")
+    private CaseDetails retrieveFallback(
+        String jurisdiction,
+        String caseRef,
+        CcdAuthenticator authenticator,
+        Throwable exception
+    ) {
+        String message = exception.getMessage();
+
+        if (exception instanceof FeignException) {
+            HttpStatus status = HttpStatus.valueOf(((FeignException) exception).status());
+
+            logResponse(jurisdiction, caseRef, status, message);
+        } else {
+            log.error(
+                "Could not retrieve case with ref:{} for {} jurisdiction. Error: {}",
+                caseRef,
+                jurisdiction,
+                message,
+                exception
+            );
+        }
+
+        return null;
     }
 
+    private void logResponse(String jurisdiction, String caseRef, HttpStatus status, String originalMessage) {
+        if (HttpStatus.NOT_FOUND.equals(status)) {
+            log.info(
+                "Not found case ref:{} for {} jurisdiction {}. Error: {}",
+                caseRef,
+                jurisdiction,
+                originalMessage
+            );
+        } else if (status.is4xxClientError()) {
+            log.warn(
+                "Client error response {} when searching case ref:{} for {} jurisdiction. Error: {}",
+                status.value(),
+                caseRef,
+                jurisdiction,
+                originalMessage
+            );
+        } else if (status.is5xxServerError()) {
+            log.error(
+                "Server error response {} when searching case ref:{} for {} jurisdiction. Error: {}",
+                status.value(),
+                caseRef,
+                jurisdiction,
+                originalMessage
+            );
+        }
+    }
 }
