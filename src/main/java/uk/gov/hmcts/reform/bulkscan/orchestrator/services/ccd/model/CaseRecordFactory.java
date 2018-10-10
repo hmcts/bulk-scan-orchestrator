b8e550c079236f1157a2d5ac599f3e08ddedcde3
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-10-10 11:26:11 +0100

    Use efficient `EnumMap` instead

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/model/CaseRecordFactory.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/model/CaseRecordFactory.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.model;
 
+import com.google.common.collect.ImmutableMap;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.model.caserecord.ExceptionRecord;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.model.caserecord.SupplementaryEvidenceRecord;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.model.Envelope;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.model.CaseRecord.Construct;
@@ -13,12 +14,10 @@ import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.model.CaseR
 
 public final class CaseRecordFactory {
 
-    private static final Map<Record, Construct> AVAILABLE_MODELS = new HashMap<>();
-
-    static {
-        AVAILABLE_MODELS.put(Record.EXCEPTION_RECORD, ExceptionRecord::new);
-        AVAILABLE_MODELS.put(Record.SUPPLEMENTARY_EVIDENCE, SupplementaryEvidenceRecord::new);
-    }
+    private static final Map<Record, Construct> AVAILABLE_MODELS = new EnumMap<>(ImmutableMap.of(
+        Record.EXCEPTION_RECORD, ExceptionRecord::new,
+        Record.SUPPLEMENTARY_EVIDENCE, SupplementaryEvidenceRecord::new
+    ));
 
     public static CaseDataCreator getCaseDataCreator(Envelope envelope, CaseDetails caseDetails) {
         Record record;
