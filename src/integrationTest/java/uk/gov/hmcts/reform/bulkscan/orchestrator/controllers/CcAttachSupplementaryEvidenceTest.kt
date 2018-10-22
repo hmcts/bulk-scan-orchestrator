7e68cc2503c53f6d866dbb53c476aa80706ddbb9
Kevlar <kevin.j.cross@gmail.com>
2018-10-22 13:03:44 +0100

    cleaned up the tests a bit. Added error case for missing data.

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcAttachSupplementaryEvidenceTest.kt
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcAttachSupplementaryEvidenceTest.kt
@@ -4,6 +4,7 @@ import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.builder.RequestSpecBuilder
 import io.restassured.http.ContentType.JSON
+import io.restassured.specification.RequestSpecification
 import org.hamcrest.Matchers.contains
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.BeforeEach
@@ -14,6 +15,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import uk.gov.hmcts.reform.bulkscan.orchestrator.controllers.config.IntegrationTest
 import uk.gov.hmcts.reform.bulkscan.orchestrator.controllers.config.PortWaiter.waitFor
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails
 
 @ExtendWith(SpringExtension::class)
 @IntegrationTest
@@ -22,6 +24,14 @@ class CcdAttachSupplementaryEvidenceTest {
     @LocalServerPort
     private var applicationPort: Int = 0
 
+    fun RequestSpecification.postToCallback(type: String = "about-to-submit") = post("/callback/{type}", type)
+    fun RequestSpecification.setCallbackFields(init: CallbackRequest.CallbackRequestBuilder.() -> CallbackRequest.CallbackRequestBuilder)
+        : RequestSpecification {
+        val request = CallbackRequest.builder()
+        body(request.init().build())
+        return this
+    }
+
     @BeforeEach
     fun before() {
         waitFor(applicationPort)
@@ -29,20 +39,48 @@ class CcdAttachSupplementaryEvidenceTest {
     }
 
     @Test
-    fun `should be able to call the ccd event endpoint`() {
+    fun `should successfully able to call the ccd event endpoint`() {
         given()
-            .body(CallbackRequest.builder().eventId("another-id").build())
-            .post("/callback/{type}", "about-to-submit")
+            .setCallbackFields {
+                eventId("attachToExistingCase")
+                caseDetails(CaseDetails.builder().build())
+            }
+            .postToCallback()
             .then()
             .statusCode(200)
+            .log().everything()
             .body("errors.size()", equalTo(0))
     }
 
     @Test
-    fun `invalid event should return an error`() {
+    fun `should return error if case details is null`() {
         given()
-            .body(CallbackRequest.builder().eventId("another-id").build())
-            .post("/callback/{type}", "someType")
+            .setCallbackFields {
+                eventId("attachToExistingCase")
+            }
+            .postToCallback()
+            .then()
+            .statusCode(200)
+            .log().everything()
+            .body("errors", contains("Internal Error: No case details supplied"))
+    }
+
+    @Test
+    fun `invalid event type should create error`() {
+        given()
+            .setCallbackFields { eventId("another-id") }
+            .postToCallback("about-to-submit")
+            .then()
+            .statusCode(200)
+            .log().everything()
+            .body("errors", contains("Internal Error: Invalid event ID:another-id"))
+    }
+
+    @Test
+    fun `invalid type should return an error`() {
+        given()
+            .setCallbackFields { eventId("another-id") }
+            .postToCallback("someType")
             .then()
             .statusCode(200)
             .body("errors", contains("Internal Error: invalid event: someType"))
--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackProcessor.kt
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackProcessor.kt
@@ -3,13 +3,14 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest
-import uk.gov.hmcts.reform.ccd.client.model.CallbackTypes
 import uk.gov.hmcts.reform.ccd.client.model.CallbackTypes.ABOUT_TO_SUBMIT
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails
 
 private val logger = KotlinLogging.logger {}
 
 @Service
 class CallbackProcessor {
+
     /**
      * returns
      *  - a list of errors
@@ -17,6 +18,7 @@ class CallbackProcessor {
      */
     fun processEventType(type: String, callback: CallbackRequest): List<String> {
         logger.info { "Processing ccd callback, type: $type, eventId: ${callback.eventId}" }
+
         return when (type) {
             ABOUT_TO_SUBMIT -> aboutToSubmit(callback)
             else -> {
@@ -26,5 +28,26 @@ class CallbackProcessor {
         }
     }
 
-    private fun aboutToSubmit(callback: CallbackRequest): List<String> = listOf()
+    private fun aboutToSubmit(callback: CallbackRequest): List<String> {
+        return when (callback.eventId) {
+            "attachToExistingCase" -> attachToExistingCase(callback)
+            else -> {
+                logger.error { "Invalid event ID:${callback.eventId}" }
+                listOf("Internal Error: Invalid event ID:${callback.eventId}")
+            }
+        }
+    }
+
+    private fun attachToExistingCase(callback: CallbackRequest): List<String> {
+        return if (callback.caseDetails != null) {
+            handleAttachRecord(callback.caseDetails)
+        } else {
+            logger.error { "No case details supplied eventId: ${callback.eventId}" }
+            listOf("Internal Error: No case details supplied")
+        }
+    }
+
+    private fun handleAttachRecord(caseDetails: CaseDetails): List<String> {
+        return listOf()
+    }
 }
