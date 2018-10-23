bee1735dfe727fe3522bd9217349df9fec69ee07
Kevlar <kevin.j.cross@gmail.com>
2018-10-23 16:45:55 +0100

    refactor to make it easier to review

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcAttachSupplementaryEvidenceTest.kt
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcAttachSupplementaryEvidenceTest.kt
@@ -29,6 +29,7 @@ import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse
 @ExtendWith(SpringExtension::class)
 @IntegrationTest
 class CcdAttachSupplementaryEvidenceTest {
+
     companion object {
         private val mapper: ObjectMapper = ObjectMapper()
         fun asJson(obj: Any): String = mapper.writeValueAsString(obj)
@@ -37,21 +38,24 @@ class CcdAttachSupplementaryEvidenceTest {
     @LocalServerPort
     private var applicationPort: Int = 0
 
-    @Value("\${wiremock.port}")
-    private var wireMockPort: Int = 0
-
-    private fun RequestSpecification.postToCallback(type: String = "about-to-submit") = post("/callback/{type}", type)
-    private fun RequestSpecification.setCallbackFields(init: CallbackRequest.CallbackRequestBuilder.() -> CallbackRequest.CallbackRequestBuilder)
+    fun RequestSpecification.postToCallback(type: String = "about-to-submit") = post("/callback/{type}", type)
+    fun RequestSpecification.setCallbackFields(init: CallbackRequest.CallbackRequestBuilder.() -> CallbackRequest.CallbackRequestBuilder)
         : RequestSpecification {
         val request = CallbackRequest.builder().eventId("attachToExistingCase")
         body(request.init().build())
         return this
     }
 
+    @Value("\${wiremock.port}")
+    private var wireMockPort: Int = 0
+    private val wireMock by lazy { WireMock(wireMockPort) }
+
+    private val startEvent = get("/caseworkers/640/jurisdictions/BULKSCAN/case-types" +
+        "/Bulk_Scanned/cases/someCase/event-triggers/TBD/token")
+
     private fun baseCase() = CaseDetails.builder().caseTypeId(CASE_TYPE).jurisdiction(JURIDICTION)
     private fun nullCaseData() = baseCase().data(null).build()
     private fun successfulCase() = baseCase().data(mapOf("attachToCaseReference" to "someCase")).build()
-
 
     @BeforeEach
     fun before() {
@@ -61,8 +65,7 @@ class CcdAttachSupplementaryEvidenceTest {
 
     @Test
     fun `should successfully able to call the ccd event endpoint`() {
-        wireMock().register(startEvent()
-            .willReturn(okJson(asJson(StartEventResponse.builder().build()))))
+        wireMock.register(startEvent.willReturn(okJson(asJson(StartEventResponse.builder().build()))))
         given()
             .setCallbackFields { caseDetails(successfulCase()) }
             .postToCallback()
@@ -74,21 +77,14 @@ class CcdAttachSupplementaryEvidenceTest {
 
     @Test
     fun `Should return error if 404 returned`() {
-        wireMock().register(startEvent()
-            .willReturn(status(404)))
+        wireMock.register(startEvent.willReturn(status(404)))
         given()
             .setCallbackFields { caseDetails(successfulCase()) }
             .postToCallback()
             .then()
             .statusCode(200)
-            .log().everything()
-            .body("errors.size()", equalTo(0))
+            .body("errors", contains("Internal Error: response 404 submitting event"))
     }
-
-    private fun wireMock() = WireMock(wireMockPort)
-
-    private fun startEvent() =
-        get("/caseworkers/640/jurisdictions/BULKSCAN/case-types/Bulk_Scanned/cases/someCase/event-triggers/TBD/token")
 
     @Test
     fun `should return error when fails when case data is null`() {
@@ -99,7 +95,6 @@ class CcdAttachSupplementaryEvidenceTest {
             .statusCode(200)
             .body("errors", contains("Internal Error: no case data"))
     }
-
 
     @Test
     fun `should return error if case details is null`() {
