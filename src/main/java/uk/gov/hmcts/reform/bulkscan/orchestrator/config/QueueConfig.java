d5118f64b5cca17fe3c1addd739162d33d1b30d6
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-10-25 23:39:17 +0100

    Register message handler

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/QueueConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/QueueConfig.java
@@ -1,35 +1,28 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
 import com.microsoft.azure.servicebus.IMessageHandler;
-import com.microsoft.azure.servicebus.MessageHandlerOptions;
 import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.time.Duration;
 import javax.annotation.PostConstruct;
 
 @Configuration
 @Profile("!integration")
 public class QueueConfig {
-    @Autowired
-    QueueClient client;
-    @Autowired
-    IMessageHandler messageHandler;
-    @Value("${queue.connections.max:1}")
-    private int maxServiceBusConnections;
+
+    private final QueueClient client;
+    private final IMessageHandler messageHandler;
+
+    public QueueConfig(QueueClient client, IMessageHandler messageHandler) {
+        this.client = client;
+        this.messageHandler = messageHandler;
+    }
 
     @PostConstruct
     public void initialise() throws ServiceBusException, InterruptedException {
-        MessageHandlerOptions handlerOptions = new MessageHandlerOptions(
-            maxServiceBusConnections,
-            true,
-            Duration.ofMinutes(1)
-        );
-
-        client.registerMessageHandler(messageHandler, handlerOptions);
+        client.registerMessageHandler(messageHandler);
     }
+
 }
