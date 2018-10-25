122b81b8d9a0f4c89850091ea937e1e17490d101
Kevlar <kevin.j.cross@gmail.com>
2018-10-25 15:22:43 +0100

    added Event id check.

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/AttachExceptionRecordToExistingCaseTest.kt
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/AttachExceptionRecordToExistingCaseTest.kt
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import u.gov.hmcts.reform.bulkscan.orchestrator.controllers.config.PortWaiter.waitFor
 import uk.gov.hmcts.reform.bulkscan.orchestrator.controllers.config.IntegrationTest
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest
+import uk.gov.hmcts.reform.ccd.client.model.CallbackTypes
 
 @ExtendWith(SpringExtension::class)
 @IntegrationTest
@@ -28,19 +29,40 @@ class AttachExceptionRecordToExistingCaseTest {
         RestAssured.requestSpecification = RequestSpecBuilder().setPort(applicationPort).setContentType(JSON).build()
     }
 
+
+    private val request = CallbackRequest.builder().eventId(CallbackTypes.ABOUT_TO_SUBMIT)
+
     @Test
-    fun `should be able to call the ccd event endpoint`() {
+    fun `should successfully callback with correct information`() {
         given()
-            .body(CallbackRequest.builder().build())
-            .post("/callback/{type}", "attachToCaseReference")
+            .body(request.build())
+            .post("/callback/{type}", "attach_case")
             .then()
             .statusCode(200)
             .body("errors.size()", equalTo(0))
     }
     @Test
+    fun `should fail if invalid eventId set`() {
+        given()
+            .body(request.eventId("invalid").build())
+            .post("/callback/{type}", "attach_case")
+            .then()
+            .statusCode(200)
+            .body("errors",contains("Internal Error: : event-id: invalid invalid"))
+    }
+    @Test
+    fun `should fail if no eventId set`() {
+        given()
+            .body(request.eventId(null).build())
+            .post("/callback/{type}", "attach_case")
+            .then()
+            .statusCode(200)
+            .body("errors",contains("Internal Error: : event-id: null invalid"))
+    }
+    @Test
     fun `should create error if type in incorrect`() {
         given()
-            .body(CallbackRequest.builder().build())
+            .body(request.build())
             .post("/callback/{type}", "someType")
             .then()
             .statusCode(200)
--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackProcessorImpl.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackProcessorImpl.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd;
 
 import com.google.common.collect.ImmutableList;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackTypes;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
 import java.util.List;
@@ -14,7 +15,12 @@ public class CallbackProcessorImpl implements CallbackProcessor {
     @Override
     public List<String> process(String type, String eventId, CaseDetails caseDetails) {
         if(isAttachEvent(type)) {
-            return ImmutableList.of();
+            if (CallbackTypes.ABOUT_TO_SUBMIT.equals(eventId)) {
+                return ImmutableList.of();
+            }else{
+                return ImmutableList.of(format("Internal Error: : event-id: %s invalid",eventId));
+            }
+
         }else{
             return ImmutableList.of(format("Internal Error: invalid type supplied: %s",type));
         }
