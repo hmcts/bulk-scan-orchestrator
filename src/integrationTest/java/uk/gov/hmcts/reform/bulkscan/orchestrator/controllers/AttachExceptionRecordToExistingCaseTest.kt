3bf287b87fe732a494d72a61dbdebae6e8a3d24f
Kevlar <kevin.j.cross@gmail.com>
2018-10-25 17:56:10 +0100

    review comments

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/AttachExceptionRecordToExistingCaseTest.kt
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/AttachExceptionRecordToExistingCaseTest.kt
@@ -29,7 +29,6 @@ class AttachExceptionRecordToExistingCaseTest {
         RestAssured.requestSpecification = RequestSpecBuilder().setPort(applicationPort).setContentType(JSON).build()
     }
 
-
     private val request = CallbackRequest.builder().eventId(CallbackTypes.ABOUT_TO_SUBMIT)
 
     @Test
@@ -41,6 +40,7 @@ class AttachExceptionRecordToExistingCaseTest {
             .statusCode(200)
             .body("errors.size()", equalTo(0))
     }
+
     @Test
     fun `should fail if invalid eventId set`() {
         given()
@@ -48,8 +48,9 @@ class AttachExceptionRecordToExistingCaseTest {
             .post("/callback/{type}", "attach_case")
             .then()
             .statusCode(200)
-            .body("errors",contains("Internal Error: event-id: invalid invalid"))
+            .body("errors", contains("Internal Error: event-id: invalid invalid"))
     }
+
     @Test
     fun `should fail if no eventId set`() {
         given()
@@ -57,8 +58,9 @@ class AttachExceptionRecordToExistingCaseTest {
             .post("/callback/{type}", "attach_case")
             .then()
             .statusCode(200)
-            .body("errors",contains("Internal Error: event-id: null invalid"))
+            .body("errors", contains("Internal Error: event-id: null invalid"))
     }
+
     @Test
     fun `should create error if type in incorrect`() {
         given()
