061001525650882e1bcc43393cabb37bcdb59c78
Kevlar <kevin.j.cross@gmail.com>
2018-10-25 15:07:22 +0100

    checkstyle fix.

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/RootControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/RootControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CallbackProcessor;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CallbackProcessorImpl;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
