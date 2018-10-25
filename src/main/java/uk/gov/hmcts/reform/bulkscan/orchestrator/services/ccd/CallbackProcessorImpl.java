c8e7111956f64275b0694f83a7fd9c0352aa63fe
Kevlar <kevin.j.cross@gmail.com>
2018-10-25 16:47:43 +0100

    refactored the internal errors

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackProcessorImpl.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackProcessorImpl.java
@@ -14,15 +14,15 @@ public class CallbackProcessorImpl implements CallbackProcessor {
 
     @Override
     public List<String> process(String type, String eventId, CaseDetails caseDetails) {
-        if(isAttachEvent(type)) {
+        if (isAttachEvent(type)) {
             if (CallbackTypes.ABOUT_TO_SUBMIT.equals(eventId)) {
                 return ImmutableList.of();
-            }else{
-                return ImmutableList.of(format("Internal Error: : event-id: %s invalid",eventId));
+            } else {
+                return ImmutableList.of(format("Internal Error: : event-id: %s invalid", eventId));
             }
 
-        }else{
-            return ImmutableList.of(format("Internal Error: invalid type supplied: %s",type));
+        } else {
+            return ImmutableList.of(format("Internal Error: invalid type supplied: %s", type));
         }
     }
 
