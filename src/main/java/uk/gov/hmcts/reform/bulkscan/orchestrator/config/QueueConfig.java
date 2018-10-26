575fa93ac36b0bb45445848ff52bd8d308bb8c0d
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-10-26 13:19:23 +0100

    Use thread pool to read from queue

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/QueueConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/QueueConfig.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.microsoft.azure.servicebus.IMessageHandler;
 import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 @Configuration
 @ConditionalOnExpression("#{!environment.getProperty('spring.profiles.active').contains('integration') && !environment.getProperty('spring.profiles.active').contains('functional')}")
@@ -22,7 +26,13 @@ public class QueueConfig {
 
     @PostConstruct
     public void initialise() throws ServiceBusException, InterruptedException {
-        client.registerMessageHandler(messageHandler);
+        // Note: single threaded queue reader for now to keep it simple.
+        ThreadFactory namedThreadFactory =
+            new ThreadFactoryBuilder().setNameFormat("queue-reader-%d").build();
+        ExecutorService executorService = Executors.newSingleThreadExecutor(
+            namedThreadFactory
+        );
+        client.registerMessageHandler(messageHandler, executorService);
     }
 
 }
