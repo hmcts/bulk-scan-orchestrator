1c21c3748df92482df304075e31c86b91178679f
Kevin Cross <kevin.j.cross@gmail.com>
2018-10-26 14:11:19 +0100

    RPE-670: new service addded(#106)

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/AttachExceptionRecordToExistingCaseTest.kt
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/AttachExceptionRecordToExistingCaseTest.kt
@@ -4,6 +4,8 @@ import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.builder.RequestSpecBuilder
 import io.restassured.http.ContentType.JSON
+import org.hamcrest.Matchers.contains
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -30,8 +32,18 @@ class AttachExceptionRecordToExistingCaseTest {
     fun `should be able to call the ccd event endpoint`() {
         given()
             .body(CallbackRequest.builder().build())
+            .post("/callback/{type}", "attach_case")
+            .then()
+            .statusCode(200)
+            .body("errors.size()", equalTo(0))
+    }
+    @Test
+    fun `should create error if type in incorrect`() {
+        given()
+            .body(CallbackRequest.builder().build())
             .post("/callback/{type}", "someType")
             .then()
             .statusCode(200)
+            .body("errors", contains("Internal Error: invalid type supplied: someType"))
     }
 }
