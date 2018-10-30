929b870d93025f71d786b85de7e2f8ed376d8e6a
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-10-30 11:53:22 +0000

    Remove unused code

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/MessageSender.kt
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/MessageSender.kt
@@ -2,12 +2,9 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.controllers
 
 import com.microsoft.azure.servicebus.IMessageHandler
 import com.microsoft.azure.servicebus.Message
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 
 class MessageSender(@Autowired private val processor: IMessageHandler) {
-    val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
     fun send(message: Message) {
         processor.onMessageAsync(message).get()
     }
