864e9e3e49ffbbb3c181e3a1120377e658bd3db9
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-10-31 16:37:41 +0000

    Servicebus initialised with retry

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/QueueConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/QueueConfig.java
@@ -1,15 +1,19 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.microsoft.azure.servicebus.IMessageHandler;
 import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
 
 /**
@@ -19,6 +23,8 @@ import javax.annotation.PostConstruct;
 @Configuration
 @Profile("!nosb") // do not register handler for the nosb (test) profile
 public class QueueConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(QueueConfig.class);
 
     private final QueueClient client;
     private final IMessageHandler messageHandler;
@@ -36,7 +42,23 @@ public class QueueConfig {
         ExecutorService executorService = Executors.newSingleThreadExecutor(
             namedThreadFactory
         );
-        client.registerMessageHandler(messageHandler, executorService);
+
+        // Note: lazy init with retry otherwise AKS setup fails as a queue is
+        // created in that environment only after deployment is complete.
+        int tries = 0;
+        while (true) {
+            try {
+                client.registerMessageHandler(messageHandler, executorService);
+                return;
+            } catch (Throwable t) {
+                tries++;
+                if (tries >= 5) {
+                    throw t;
+                }
+                log.info("Register handler error: {}. Retrying...", t.getMessage());
+                Uninterruptibles.sleepUninterruptibly(10 * tries, TimeUnit.SECONDS);
+            }
+        }
     }
 
 }
