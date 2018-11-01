ad05f9cad283bfea80444f0e0036020a37aa67f8
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-11-01 15:00:11 +0000

    Remove references in tf files

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -12,10 +12,6 @@ output "vaultUri" {
 
 // region: settings for functional tests
 
-output "QUEUE_READ_CONN_STRING" {
-  value = "${data.terraform_remote_state.shared_infra.queue_primary_listen_connection_string}"
-}
-
 output "QUEUE_WRITE_CONN_STRING" {
   value = "${data.terraform_remote_state.shared_infra.queue_primary_send_connection_string}"
 }
