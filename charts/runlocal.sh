d9487629948442850b43d033f5bfe8bc06f4cda8
Kevlar <kevin.j.cross@gmail.com>
2018-12-07 18:31:05 +0000

    ipdated to latest to use the passing branch build.

--- charts/runlocal.sh
+++ charts/runlocal.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eu
-chart=bulk-scan-orchestrator/
+chart=./bulk-scan-orchestrator/
 template=$chart/values.template.yaml
 
 export IDAM_USER_NAME=bulkscan+ccd@gmail.com
 export IDAM_USER_PASSWORD=Password12
 export SERVICE_NAME=$1
-export IMAGE_NAME=hmcts.azurecr.io/bulk-scan-orchestrator@sha256:85dd5e1b1054c68aaa5e356ee07de2e0d9be5b1540d82b8466d3f2106ace3c9a
+export IMAGE_NAME=hmcts.azurecr.io/hmcts/bulk-scan-orchestrator:latest
 export SERVICE_FQDN=${SERVICE_NAME}.service.core-compute-saat.internal
 export S2S_SECRET="AAAA"
 export IDAM_CLIENT_SECRET="AAAA"
@@ -14,8 +14,8 @@ export IDAM_USERS_BULKSCAN_USERNAME="bulkscan+ccd@gmail.com"
 export IDAM_USERS_BULKSCAN_PASSWORD="Password12"
 cat $template | envsubst > $chart/values.yaml
 
-helm dep update charts/bulk-scan-orchestrator/
+helm dep update $chart
 helm del --purge $1 || echo "No release found carrying on"
-helm install --debug charts/bulk-scan-orchestrator/ --name $1
+helm install --debug $chart --wait --name $1
 
   
