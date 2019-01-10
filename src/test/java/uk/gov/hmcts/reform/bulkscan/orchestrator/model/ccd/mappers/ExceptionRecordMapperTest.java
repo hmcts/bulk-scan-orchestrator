267251701da8df1b2b34091110acc5f548dcc8c7
Aliveni Choppa <aliveni.ch@gmail.com>
2019-01-10 15:27:03 +0000

    Added subtype to Document to display it in ccd

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/model/ccd/mappers/ExceptionRecordMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/model/ccd/mappers/ExceptionRecordMapperTest.java
@@ -77,6 +77,7 @@ public class ExceptionRecordMapperTest {
                     scannedDocument.fileName,
                     scannedDocument.controlNumber,
                     scannedDocument.type,
+                    scannedDocument.subtype,
                     scannedDocument.scannedDate.atZone(ZoneId.systemDefault()).toInstant(),
                     scannedDocument.url.documentUrl
                 )
