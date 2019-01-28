3d94bff4ed4b4cca9c5602274fd52a72fff0a694
Donatas Martinkus <donatas.martinkus@hmcts.net>
2019-01-28 11:29:27 +0000

    Add some logging

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/endpoints/JurisdictionEndpoint.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/endpoints/JurisdictionEndpoint.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.endpoints;
 
 import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
@@ -23,6 +25,8 @@ import static java.util.Map.Entry;
 @Endpoint(id = "jurisdictions")
 @EnableConfigurationProperties(JurisdictionToUserMapping.class)
 public class JurisdictionEndpoint {
+
+    private static final Logger log = LoggerFactory.getLogger(JurisdictionEndpoint.class);
 
     private final Map<String, Credential> jurisdictions;
     private final Function<String, Credential> credentialProvider;
@@ -61,12 +65,18 @@ public class JurisdictionEndpoint {
         try {
             idamClient.authenticateUser(credential.getUsername(), credential.getPassword());
 
+            log.debug("Successful authentication");
+
             return HttpStatus.OK;
         } catch (FeignException exception) {
+            log.warn("An error occurred while authenticating {}", credential.getUsername(), exception);
+
             // in current state unable to set response code <200 to test such situation
             // but running manually received `0` status code which crashed the endpoint response itself
             return Optional.ofNullable(HttpStatus.resolve(exception.status())).orElse(HttpStatus.UNAUTHORIZED);
         } catch (Exception exception) {
+            log.error("An error occurred while authenticating {}", credential.getUsername(), exception);
+
             return HttpStatus.UNAUTHORIZED;
         }
     }
