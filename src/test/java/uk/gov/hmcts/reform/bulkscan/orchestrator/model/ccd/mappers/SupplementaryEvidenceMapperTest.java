4e1d7da6c1a9e273294824f76f31b8b34d4b2952
Rafal Kondratowicz <rkondr@gmail.com>
2019-02-14 11:30:11 +0000

    Don't add supplementary evidence with the same control number

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/model/ccd/mappers/SupplementaryEvidenceMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/model/ccd/mappers/SupplementaryEvidenceMapperTest.java
@@ -70,7 +70,7 @@ public class SupplementaryEvidenceMapperTest {
         List<Document> envelopeDocs =
             asList(
                 new Document("a1.pdf", "aaa1", "type_a1", "subtype_a1", now().plusSeconds(3), "http://localhost/a.pdf"), // same url!
-                new Document("b.pdf", "bbb", "type_b", "subtype_b", now().plusSeconds(4), "http://localhost/xxxxx.pdf")  // same name etc, but different url
+                new Document("b.pdf", "bbb1", "type_b", "subtype_b", now().plusSeconds(4), "http://localhost/xxxxx.pdf")
             );
 
         // when
@@ -84,9 +84,38 @@ public class SupplementaryEvidenceMapperTest {
                 tuple(ccdDoc.value.fileName, ccdDoc.value.url.documentUrl)
             )
             .containsExactly(
-                tuple("a.pdf","http://localhost/a.pdf"),
-                tuple("b.pdf","http://localhost/b.pdf"),
-                tuple("b.pdf","http://localhost/xxxxx.pdf")
+                tuple("a.pdf", "http://localhost/a.pdf"),
+                tuple("b.pdf", "http://localhost/b.pdf"),
+                tuple("b.pdf", "http://localhost/xxxxx.pdf")
+            );
+    }
+
+    @Test
+    public void should_not_add_document_from_envelope_if_document_with_the_same_control_number_is_already_present_in_case() {
+        // given
+        List<Document> existingDocs =
+            asList(
+                new Document("a.pdf", "AAA", "type_a", "subtype_a", now().plusSeconds(1), "http://localhost/a.pdf"),
+                new Document("b.pdf", "BBB", "type_b", "subtype_b", now().plusSeconds(2), "http://localhost/b.pdf")
+            );
+
+        List<Document> envelopeDocs =
+            asList(
+                new Document("c.pdf", "AAA", "type_c", "subtype_c", now().plusSeconds(3), "http://localhost/c.pdf"), // same control number!
+                new Document("d.pdf", "DDD", "type_d", "subtype_d", now().plusSeconds(4), "http://localhost/d.pdf")
+            );
+
+        // when
+        SupplementaryEvidence result = mapper.map(existingDocs, envelopeDocs);
+
+        // then
+        assertThat(result.scannedDocuments).hasSize(3); // only one doc should be added
+        assertThat(result.scannedDocuments)
+            .extracting(ccdDoc -> tuple(ccdDoc.value.fileName, ccdDoc.value.controlNumber))
+            .containsExactly(
+                tuple("a.pdf", "AAA"),
+                tuple("b.pdf", "BBB"),
+                tuple("d.pdf", "DDD")
             );
     }
 
