3030205a3dff31a95d6231d71c84a5cd7efa220e
Rafal Kondratowicz <rkondr@gmail.com>
2019-02-27 16:03:40 +0000

    Clean up CcdCallbackController

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcdCallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcdCallbackController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.AttachCaseCallbackService;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
@@ -12,12 +13,12 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 
 import java.util.List;
-import javax.validation.constraints.NotNull;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
+@RequestMapping(path = "/callback", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 public class CcdCallbackController {
 
     private final AttachCaseCallbackService attachCaseCallbackService;
@@ -27,26 +28,18 @@ public class CcdCallbackController {
         this.attachCaseCallbackService = attachCaseCallbackService;
     }
 
-    @PostMapping(
-        path = "/callback/attach_case",
-        consumes = APPLICATION_JSON_VALUE,
-        produces = APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<CallbackResponse> handleCallback(@RequestBody CallbackRequest callback) {
+    @PostMapping(path = "/attach_case")
+    public ResponseEntity<CallbackResponse> attachToCase(@RequestBody CallbackRequest callback) {
         if (callback != null && callback.getCaseDetails() != null) {
-            return respondWithErrorList(attachCaseCallbackService.process(callback.getCaseDetails()));
+            List<String> errors = attachCaseCallbackService.process(callback.getCaseDetails());
+            return ok().body(response(errors));
         } else {
-            return respondWithErrorList(ImmutableList.of("Internal Error: callback or case details were empty"));
+            return ok().body(response(ImmutableList.of("Internal Error: callback or case details were empty")));
         }
     }
 
-    @NotNull
-    private ResponseEntity<CallbackResponse> respondWithErrorList(List<String> errors) {
-        return ok()
-            .body(AboutToStartOrSubmitCallbackResponse
-                .builder()
-                .errors(errors)
-                .build());
+    private AboutToStartOrSubmitCallbackResponse response(List<String> errors) {
+        return AboutToStartOrSubmitCallbackResponse.builder().errors(errors).build();
     }
 }
 
