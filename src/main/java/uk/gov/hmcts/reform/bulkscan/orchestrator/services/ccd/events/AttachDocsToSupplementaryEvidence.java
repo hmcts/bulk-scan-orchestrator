299d6d3a6a647377e233581e4f512065f970dbed
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-03-06 18:22:49 +0000

    Do not start a ccd event when envelope contains only duplicates (#311)

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/AttachDocsToSupplementaryEvidence.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/AttachDocsToSupplementaryEvidence.java
@@ -1,15 +1,20 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.events;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.model.ccd.CaseData;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.model.ccd.mappers.SupplementaryEvidenceMapper;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.model.Envelope;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.helper.ScannedDocumentsHelper.getDocuments;
 
 @Component
 class AttachDocsToSupplementaryEvidence extends AbstractEventPublisher {
+
+    private static final Logger log = LoggerFactory.getLogger(AttachDocsToSupplementaryEvidence.class);
 
     public static final String EVENT_TYPE_ID = "attachScannedDocs";
     public static final String EVENT_SUMMARY = "Attach scanned documents";
@@ -20,8 +25,12 @@ class AttachDocsToSupplementaryEvidence extends AbstractEventPublisher {
         this.mapper = mapper;
     }
 
-    public void publish(Envelope envelope, String caseTypeId) {
-        publish(envelope, caseTypeId, EVENT_TYPE_ID, EVENT_SUMMARY);
+    public void publish(Envelope envelope, CaseDetails existingCase) {
+        if (mapper.getDocsToAdd(getDocuments(existingCase), envelope.documents).isEmpty()) {
+            log.warn("Envelope {} has no new documents. CCD Case {} not updated", envelope.id, existingCase.getId());
+        } else {
+            publish(envelope, existingCase.getCaseTypeId(), EVENT_TYPE_ID, EVENT_SUMMARY);
+        }
     }
 
     @Override
