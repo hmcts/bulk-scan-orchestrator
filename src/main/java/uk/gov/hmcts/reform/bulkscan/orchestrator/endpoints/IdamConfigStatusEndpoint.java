b5647e2dfa17fff53837e32adb7e7385707819be
leszekg <gonczar@gmail.com>
2019-03-18 16:34:10 +0000

    Extract a tool for checking jurisdiction-specific authentication in Idam
    This functionality is needed to be accessible from code, as the status will determine whether to pause processing messages or not.

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/endpoints/IdamConfigStatusEndpoint.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/endpoints/IdamConfigStatusEndpoint.java
@@ -1,83 +1,31 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.endpoints;
 
-import feign.FeignException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.model.out.JurisdictionConfigurationStatus;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.Credential;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.JurisdictionToUserMapping;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.NoUserConfiguredException;
-import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.AuthenticationChecker;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @Endpoint(id = "idam-config-status")
-@EnableConfigurationProperties(JurisdictionToUserMapping.class)
 public class IdamConfigStatusEndpoint {
 
-    private static final Logger log = LoggerFactory.getLogger(IdamConfigStatusEndpoint.class);
+    private final AuthenticationChecker authenticationChecker;
 
-    private final JurisdictionToUserMapping jurisdictionMapping;
-    private final IdamClient idamClient;
-
-    public IdamConfigStatusEndpoint(
-        JurisdictionToUserMapping mapping,
-        IdamClient idamClient
-    ) {
-        jurisdictionMapping = mapping;
-        this.idamClient = idamClient;
+    public IdamConfigStatusEndpoint(AuthenticationChecker authenticationChecker) {
+        this.authenticationChecker = authenticationChecker;
     }
 
     @ReadOperation
     public List<JurisdictionConfigurationStatus> jurisdictions() {
-        return jurisdictionMapping.getUsers()
-            .entrySet()
-            .stream()
-            .map(entry -> checkCredentials(entry.getKey(), entry.getValue()))
-            .collect(Collectors.toList());
+        return authenticationChecker.checkSignInForAllJurisdictions();
     }
 
     @ReadOperation
     public JurisdictionConfigurationStatus jurisdiction(@Selector String jurisdiction) {
-        try {
-            return checkCredentials(jurisdiction, jurisdictionMapping.getUser(jurisdiction));
-        } catch (NoUserConfiguredException exception) {
-            return new JurisdictionConfigurationStatus(jurisdiction, false, exception.getMessage());
-        }
-    }
-
-    private JurisdictionConfigurationStatus checkCredentials(String jurisdiction, Credential credential) {
-        try {
-            idamClient.authenticateUser(credential.getUsername(), credential.getPassword());
-
-            log.debug("Successful authentication of {} jurisdiction", jurisdiction);
-
-            return new JurisdictionConfigurationStatus(jurisdiction, true);
-        } catch (FeignException exception) {
-            log.warn(
-                "An error occurred while authenticating {} jurisdiction with {} username",
-                jurisdiction,
-                credential.getUsername(),
-                exception
-            );
-
-            return new JurisdictionConfigurationStatus(jurisdiction, false, exception.getMessage());
-        } catch (Exception exception) {
-            log.error(
-                "An error occurred while authenticating {} jurisdiction with {} username",
-                jurisdiction,
-                credential.getUsername(),
-                exception
-            );
-
-            return new JurisdictionConfigurationStatus(jurisdiction, false, exception.getMessage());
-        }
+        return authenticationChecker.checkSignInForJurisdiction(jurisdiction);
     }
 }
