331971e92da85b4caf4a4b893be0b373f4759dd8
leszekg <gonczar@gmail.com>
2019-04-09 15:17:51 +0100

    Provide mock message receiver for functional tests's Spring Boot context

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/FunctionalQueueConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/FunctionalQueueConfig.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator;
 
+import com.microsoft.azure.servicebus.IMessageReceiver;
 import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.IProcessedEnvelopeNotifier;
+
+import static org.mockito.Mockito.mock;
 
 public class FunctionalQueueConfig {
 
@@ -27,5 +30,11 @@ public class FunctionalQueueConfig {
     IProcessedEnvelopeNotifier testProcessedEnvelopeNotifier() {
         // return implementation that does nothing
         return envelopeId -> { };
+    }
+
+    @Bean
+    @Profile("nosb") // apply only when Service Bus should not be used
+    IMessageReceiver testMessageReceiver() {
+        return mock(IMessageReceiver.class);
     }
 }
