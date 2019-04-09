a00870e552ca24eeb8bd82f7cd72e3780d43b0d1
leszekg <gonczar@gmail.com>
2019-04-09 14:07:09 +0100

    Remove unused constructor

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/exceptions/MessageProcessingException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/exceptions/MessageProcessingException.java
@@ -5,8 +5,4 @@ public class MessageProcessingException extends RuntimeException {
     public MessageProcessingException(String message) {
         super(message);
     }
-
-    public MessageProcessingException(String message, Throwable cause) {
-        super(message);
-    }
 }
