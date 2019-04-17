444b870d05fbc6378384a750b332b3c1fc149c85
donatas M <1420191+doncem@users.noreply.github.com>
2019-04-17 13:38:03 +0100

    Specify tomcat major version for webapp (#407)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -93,6 +93,7 @@ module "bulk-scan-orchestrator" {
   asp_name                        = "${var.product}-${var.env}"
   asp_rg                          = "${var.product}-${var.env}"
   instance_size                   = "${local.sku_size}"
+  java_container_version          = "9.0"
 
   app_settings = "${merge(local.core_app_settings, local.users_usernames_settings, local.users_passwords_settings)}"
 }
