5069d1c09fdf26844e48d7143050a19941f2fadc
leszekg <gonczar@gmail.com>
2019-04-26 03:28:40 +0100

    WIP

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/helper/CaseSearcher.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/helper/CaseSearcher.java
@@ -41,7 +41,7 @@ public class CaseSearcher {
             authenticator.getUserToken(),
             authenticator.getServiceToken(),
             caseTypeId,
-            "{ \"query\":{ \"bool\":{ \"filter\":{ \"term\":{ \"data.TextField\":\"TESTPO\"}}}}}"
+            "{ \"query\":{ \"match\":{ \"data.poBox\":\"TESTPO\"}}}"
         );
 
         //    "{ \"query\": { \"match_all\": {} }, \"size\": 50}"
