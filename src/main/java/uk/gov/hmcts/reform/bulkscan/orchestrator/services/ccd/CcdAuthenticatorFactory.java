da75badd17329f7d1bbff2c2a9b21b729ada854b
Rafal Kondratowicz <rkondr@gmail.com>
2019-06-05 11:21:42 +0200

    Clean up CcdAuthenitcator a bit

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CcdAuthenticatorFactory.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CcdAuthenticatorFactory.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
@@ -14,15 +12,15 @@ import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 @EnableConfigurationProperties(JurisdictionToUserMapping.class)
 public class CcdAuthenticatorFactory {
 
-    private static final Logger log = LoggerFactory.getLogger(CcdAuthenticatorFactory.class);
-
     private final AuthTokenGenerator s2sTokenGenerator;
     private final IdamClient idamClient;
     private final JurisdictionToUserMapping users;
 
-    public CcdAuthenticatorFactory(AuthTokenGenerator s2sTokenGenerator,
-                                   IdamClient idamClient,
-                                   JurisdictionToUserMapping users) {
+    public CcdAuthenticatorFactory(
+        AuthTokenGenerator s2sTokenGenerator,
+        IdamClient idamClient,
+        JurisdictionToUserMapping users
+    ) {
         this.s2sTokenGenerator = s2sTokenGenerator;
         this.idamClient = idamClient;
         this.users = users;
@@ -30,12 +28,15 @@ public class CcdAuthenticatorFactory {
 
     public CcdAuthenticator createForJurisdiction(String jurisdiction) {
         Credential user = users.getUser(jurisdiction);
-        log.debug("Authenticating user: {}", user.getUsername());
         String userToken = idamClient.authenticateUser(user.getUsername(), user.getPassword());
         UserDetails userDetails = idamClient.getUserDetails(userToken);
 
         //TODO: RPE-738 the userToken needs a to be cached and timed-out.
         // this can be decorated here like the s2sTokenGenerator
-        return CcdAuthenticator.from(s2sTokenGenerator::generate, userDetails, () -> userToken);
+        return new CcdAuthenticator(
+            s2sTokenGenerator::generate,
+            userDetails,
+            () -> userToken
+        );
     }
 }
