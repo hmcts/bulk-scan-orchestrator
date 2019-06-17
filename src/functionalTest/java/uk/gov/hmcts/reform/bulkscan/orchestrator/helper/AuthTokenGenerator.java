1d9703d1e287648473a6b63fbe59419286d9693e
nitinprabhu <prabhunitin61@gmail.com>
2019-06-17 11:42:25 +0100

    Added annotation for autowiring helper class

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/helper/AuthTokenGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/helper/AuthTokenGenerator.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.helper;
 
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdAuthenticator;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdAuthenticatorFactory;
 
+@Service
 public class AuthTokenGenerator {
 
     private final CcdAuthenticatorFactory ccdAuthenticatorFactory;
