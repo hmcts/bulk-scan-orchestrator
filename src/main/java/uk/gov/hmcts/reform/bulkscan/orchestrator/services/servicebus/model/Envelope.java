b01faeca74ac0411d293a129f2f045130a2cdd48
Leszek Gonczar <gonczar@gmail.com>
2019-08-13 16:34:13 +0100

    Extend queue message by OCR validation warnings (#525)

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/model/Envelope.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/model/Envelope.java
@@ -21,6 +21,7 @@ public class Envelope {
     public final Classification classification;
     public final List<Document> documents;
     public final List<OcrDataField> ocrData;
+    public final List<String> ocrValidationWarnings;
 
     public Envelope(
         @JsonProperty(value = "id", required = true) String id,
@@ -34,7 +35,8 @@ public class Envelope {
         @JsonProperty(value = "opening_date", required = true) Instant openingDate,
         @JsonProperty(value = "classification", required = true) Classification classification,
         @JsonProperty(value = "documents", required = true) List<Document> documents,
-        @JsonProperty(value = "ocr_data") List<OcrDataField> ocrData
+        @JsonProperty(value = "ocr_data") List<OcrDataField> ocrData,
+        @JsonProperty(value = "ocr_validation_warnings") List<String> ocrValidationWarnings
     ) {
         this.id = id;
         this.caseRef = caseRef;
@@ -48,5 +50,6 @@ public class Envelope {
         this.classification = classification;
         this.documents = documents;
         this.ocrData = ocrData;
+        this.ocrValidationWarnings = ocrValidationWarnings;
     }
 }
