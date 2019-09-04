b16bcddd751e5ba62c68f09806c57e1d6ed26509
Donatas Martinkus <doncem@users.noreply.github.com>
2019-09-04 08:53:28 +0100

    Expand scheduler bean

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/SchedulerConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/SchedulerConfig.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -7,10 +9,14 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
 
     private static final int POOL_SIZE = 10;
+    private static AtomicInteger errorCount = new AtomicInteger(0);
+    private static final Logger log = LoggerFactory.getLogger(SchedulerConfig.class);
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
@@ -21,6 +27,11 @@ public class SchedulerConfig implements SchedulingConfigurer {
     public TaskScheduler orchestratorTaskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(POOL_SIZE);
+        scheduler.setThreadNamePrefix("BSO-");
+        scheduler.setErrorHandler(t -> {
+            log.error("Unhandled exception during task. {}: {}", t.getClass(), t.getMessage(), t);
+            errorCount.incrementAndGet();
+        });
         scheduler.initialize();
 
         return scheduler;
