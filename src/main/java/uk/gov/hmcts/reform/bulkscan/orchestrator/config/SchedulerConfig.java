412f424633ce5109637d663f720cba5bbda62ce6
Donatas Martinkus <doncem@users.noreply.github.com>
2019-09-04 08:57:52 +0100

    Fully bloom the scheduler task setup

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/SchedulerConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/SchedulerConfig.java
@@ -1,15 +1,24 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
+import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
+import com.microsoft.applicationinsights.web.internal.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+import static java.time.LocalDateTime.now;
 
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
@@ -18,6 +27,13 @@ public class SchedulerConfig implements SchedulingConfigurer {
     private static AtomicInteger errorCount = new AtomicInteger(0);
     private static final Logger log = LoggerFactory.getLogger(SchedulerConfig.class);
 
+    // todo: move to global version of time handling
+    private static final Supplier<Long> CURRENT_MILLIS_SUPPLIER = () -> now()
+        .atZone(ZoneId.of("Europe/London")).toInstant().toEpochMilli();
+
+    private static final Supplier<RequestTelemetryContext> REQUEST_CONTEXT_SUPPLIER = () ->
+        new RequestTelemetryContext(CURRENT_MILLIS_SUPPLIER.get(), null);
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         taskRegistrar.setTaskScheduler(orchestratorTaskScheduler());
@@ -25,7 +41,7 @@ public class SchedulerConfig implements SchedulingConfigurer {
 
     @Bean
     public TaskScheduler orchestratorTaskScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        ThreadPoolTaskScheduler scheduler = new OrchestratorTaskScheduler();
         scheduler.setPoolSize(POOL_SIZE);
         scheduler.setThreadNamePrefix("BSO-");
         scheduler.setErrorHandler(t -> {
@@ -35,5 +51,79 @@ public class SchedulerConfig implements SchedulingConfigurer {
         scheduler.initialize();
 
         return scheduler;
+    }
+
+    /**
+     * Custom {@link ThreadPoolTaskScheduler} to be able to register scheduled tasks via AppInsights.
+     */
+    private static class OrchestratorTaskScheduler extends ThreadPoolTaskScheduler {
+
+        private static final long serialVersionUID = 8789551087577035719L;
+
+        @Override
+        public void execute(Runnable command) {
+            super.execute(new WrappedRunnable(command, REQUEST_CONTEXT_SUPPLIER.get()));
+        }
+
+        @Override
+        public ScheduledFuture<?> schedule(Runnable task, Trigger trigger) {
+            return super.schedule(new WrappedRunnable(task, REQUEST_CONTEXT_SUPPLIER.get()), trigger);
+        }
+
+        @Override
+        public ScheduledFuture<?> schedule(Runnable task, Date startTime) {
+            return super.schedule(new WrappedRunnable(task, REQUEST_CONTEXT_SUPPLIER.get()), startTime);
+        }
+
+        @Override
+        public ScheduledFuture<?> scheduleAtFixedRate(Runnable task, Date startTime, long period) {
+            return super.scheduleAtFixedRate(new WrappedRunnable(
+                task,
+                REQUEST_CONTEXT_SUPPLIER.get()
+            ), startTime, period);
+        }
+
+        @Override
+        public ScheduledFuture<?> scheduleAtFixedRate(Runnable task, long period) {
+            return super.scheduleAtFixedRate(new WrappedRunnable(task, REQUEST_CONTEXT_SUPPLIER.get()), period);
+        }
+
+        @Override
+        public ScheduledFuture<?> scheduleWithFixedDelay(Runnable task, Date startTime, long delay) {
+            return super.scheduleWithFixedDelay(new WrappedRunnable(
+                task,
+                REQUEST_CONTEXT_SUPPLIER.get()
+            ), startTime, delay);
+        }
+
+        @Override
+        public ScheduledFuture<?> scheduleWithFixedDelay(Runnable task, long delay) {
+            return super.scheduleWithFixedDelay(new WrappedRunnable(task, REQUEST_CONTEXT_SUPPLIER.get()), delay);
+        }
+    }
+
+    private static class WrappedRunnable implements Runnable {
+
+        private final Runnable task;
+        private RequestTelemetryContext requestContext;
+
+        WrappedRunnable(Runnable task, RequestTelemetryContext requestContext) {
+            this.task = task;
+            this.requestContext = requestContext;
+        }
+
+        @Override
+        public void run() {
+            if (ThreadContext.getRequestTelemetryContext() != null) {
+                ThreadContext.remove();
+
+                // since this runnable is ran on schedule, update the context on every run
+                requestContext = REQUEST_CONTEXT_SUPPLIER.get();
+            }
+
+            ThreadContext.setRequestTelemetryContext(requestContext);
+
+            task.run();
+        }
     }
 }
