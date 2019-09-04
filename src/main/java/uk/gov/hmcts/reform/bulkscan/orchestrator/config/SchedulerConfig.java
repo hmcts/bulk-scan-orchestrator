c7937d43139505ce8ff57143841baafa87f8602c
Donatas Martinkus <doncem@users.noreply.github.com>
2019-09-04 08:50:37 +0100

    Extract to separate bean

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/SchedulerConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/SchedulerConfig.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
@@ -12,10 +14,15 @@ public class SchedulerConfig implements SchedulingConfigurer {
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setTaskScheduler(orchestratorTaskScheduler());
+    }
+
+    @Bean
+    public TaskScheduler orchestratorTaskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(POOL_SIZE);
         scheduler.initialize();
 
-        taskRegistrar.setTaskScheduler(scheduler);
+        return scheduler;
     }
 }
