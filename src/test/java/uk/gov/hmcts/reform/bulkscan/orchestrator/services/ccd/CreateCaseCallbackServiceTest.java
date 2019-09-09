2bb736edada59b03317cad826522f84ac6be6fa2
Donatas Martinkus <doncem@users.noreply.github.com>
2019-09-09 11:42:32 +0100

    Add validation feature which return `ExceptionRecord` on success

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CreateCaseCallbackServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CreateCaseCallbackServiceTest.java
@@ -3,8 +3,11 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd;
 import io.vavr.control.Either;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.client.transformation.model.request.ExceptionRecord;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,10 +25,44 @@ class CreateCaseCallbackServiceTest {
     }
 
     @Test
-    void should_proceed_with_not_implemented_error() {
+    void should_report_all_errors_when_null_is_provided_as_case_details() {
         Either<List<String>, ExceptionRecord> output = SERVICE.process(null, EVENT_ID);
 
         assertThat(output.isLeft()).isTrue();
-        assertThat(output.getLeft()).containsOnly("Not yet implemented");
+        assertThat(output.getLeft()).containsOnly(
+            "Missing caseType",
+            "Missing poBox",
+            "Internal Error: invalid jurisdiction supplied: null",
+            "Missing journeyClassification",
+            "Missing deliveryDate",
+            "Missing openingDate"
+        );
+    }
+
+    @Test
+    void should_successfully_create_exception_record_with_documents_and_ocr_data_for_transformation_client() {
+        // given
+        Map<String, Object> data = new HashMap<>();
+        // putting 6 via `ImmutableMap` is available from Java 9
+        data.put("poBox", "12345");
+        data.put("journeyClassification", "EXCEPTION");
+        data.put("deliveryDate", "2019-09-06T15:30:03.000Z");
+        data.put("openingDate", "2019-09-06T15:30:04.000Z");
+        data.put("scannedDocuments", TestCaseBuilder.document("https://url", "some doc"));
+        data.put("scanOCRData", TestCaseBuilder.ocrDataEntry("some key", "some value"));
+
+        CaseDetails caseDetails = TestCaseBuilder.createCaseWith(builder -> builder
+            .caseTypeId("some case type")
+            .jurisdiction("some jurisdiction")
+            .data(data)
+        );
+
+        // when
+        Either<List<String>, ExceptionRecord> output = SERVICE.process(caseDetails, EVENT_ID);
+
+        // then
+        assertThat(output.isRight()).isTrue();
+        assertThat(output.get().scannedDocuments).hasSize(0);
+        assertThat(output.get().ocrDataFields).hasSize(0);
     }
 }
