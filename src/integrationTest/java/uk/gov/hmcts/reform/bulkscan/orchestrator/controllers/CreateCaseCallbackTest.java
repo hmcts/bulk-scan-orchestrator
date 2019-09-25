42d0e53da131e99b3616a8e37e9d0f2d002cf9c1
Donatas Martinkus <doncem@users.noreply.github.com>
2019-09-25 11:23:13 +0100

    Add integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CreateCaseCallbackTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CreateCaseCallbackTest.java
@@ -3,30 +3,33 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.client.transformation.TransformationClient;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.config.IntegrationTest;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.model.in.CcdCallbackRequest;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CreateCaseCallbackService;
 
 import java.io.IOException;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.givenThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.google.common.io.Resources.getResource;
 import static com.google.common.io.Resources.toByteArray;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.Matchers.matchesRegex;
 import static org.springframework.http.HttpStatus.OK;
 
 @IntegrationTest
 class CreateCaseCallbackTest {
+
+    private static final String CASE_ID_REGEX =
+        // will be replaced with real one after ccd api
+        "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
 
     private static final String IDAM_TOKEN = "idam-token";
     private static final String USER_ID = "user-id";
@@ -36,29 +39,40 @@ class CreateCaseCallbackTest {
     @LocalServerPort
     int serverPort;
 
-    @SpyBean
-    private TransformationClient transformationClient;
-
-    @SpyBean
-    private CreateCaseCallbackService callbackService;
-
     @Test
     void should_create_case_if_classification_new_application_with_documents_and_ocr_data() {
-        // TBD - WIP
-        // Pending fix in PR-582
-        postWithBody(getRequestBody("valid.json"))
+        setUpTransformation(getTransformationResponseBody("ok-no-warnings.json"));
+
+        postWithBody(getRequestBody("valid-new-application.json"))
             .log().all()
-            .statusCode(OK.value());
+            .statusCode(OK.value())
+            .body("errors", empty())
+            .body("warnings", empty())
+            .body("data.caseReference", matchesRegex(CASE_ID_REGEX));
     }
 
     @Test
     void should_create_case_if_classification_new_application_with_documents_and_without_ocr_data() {
-        // TBD
+        setUpTransformation(getTransformationResponseBody("ok-with-warnings.json"));
+
+        postWithBody(getRequestBody("valid-new-application-with-ocr.json"))
+            .log().all()
+            .statusCode(OK.value())
+            .body("errors", empty())
+            .body("warnings", contains("case type id looks like a number"))
+            .body("data.caseReference", matchesRegex(CASE_ID_REGEX));
     }
 
     @Test
     void should_create_case_if_classification_exception_with_documents_and_ocr_data() {
-        // TBD
+        setUpTransformation(getTransformationResponseBody("ok-no-warnings.json"));
+
+        postWithBody(getRequestBody("valid-exception.json"))
+            .log().all()
+            .statusCode(OK.value())
+            .body("errors", empty())
+            .body("warnings", empty())
+            .body("data.caseReference", matchesRegex(CASE_ID_REGEX));
     }
 
     @Test
@@ -66,8 +80,6 @@ class CreateCaseCallbackTest {
         postWithBody("{}".getBytes())
             .statusCode(OK.value())
             .body("errors", hasItem("Internal Error: callback or case details were empty"));
-
-        verify(callbackService, never()).process(any(CcdCallbackRequest.class), anyString(), anyString());
     }
 
     @Test
@@ -82,12 +94,27 @@ class CreateCaseCallbackTest {
             ));
     }
 
-    private byte[] getRequestBody(String filename) {
+    private void setUpTransformation(String responseBody) {
+        givenThat(post("/transform-exception-record")
+            .withHeader("ServiceAuthorization", containing("Bearer"))
+            .willReturn(okJson(responseBody))
+        );
+    }
+
+    private byte[] getFileContents(String ccdCallbackSubFolders, String filename) {
         try {
-            return toByteArray(getResource("ccd/callback/create-case/request/" + filename));
+            return toByteArray(getResource("ccd/callback/" + ccdCallbackSubFolders + filename));
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    private byte[] getRequestBody(String filename) {
+        return getFileContents("create-case/request/", filename);
+    }
+
+    private String getTransformationResponseBody(String filename) {
+        return new String(getFileContents("transformation-client/response/", filename));
     }
 
     private ValidatableResponse postWithBody(byte[] body) {
