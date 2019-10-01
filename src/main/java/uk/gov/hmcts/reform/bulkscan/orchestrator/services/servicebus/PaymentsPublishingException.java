d285bae30ae85700b8b4c50012447ae02292ec40
apereverzin <andrey.pereverzin@hotmail.com>
2019-10-01 09:32:18 +0100

    Suppressed serial

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/PaymentsPublishingException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/PaymentsPublishingException.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus;
 
+@SuppressWarnings("serial")
 public class PaymentsPublishingException extends RuntimeException {
 
     public PaymentsPublishingException(String message, Throwable cause) {
