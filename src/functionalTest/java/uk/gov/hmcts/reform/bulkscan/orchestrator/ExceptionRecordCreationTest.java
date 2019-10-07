ff576b2182bf094b14d7cdeff3a9841a3014717e
apereverzin <andrey.pereverzin@hotmail.com>
2019-10-07 09:49:37 +0100

    Testing

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/ExceptionRecordCreationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/ExceptionRecordCreationTest.java
@@ -96,7 +96,7 @@ class ExceptionRecordCreationTest {
         assertThat(caseDetails.getCaseTypeId()).isEqualTo("BULKSCAN_ExceptionRecord");
         assertThat(caseDetails.getJurisdiction()).isEqualTo("BULKSCAN");
 
-        Map<String, String> expectedOcrData = ImmutableMap.of("first_name", "value1", "last_name", "value2");
+        Map<String, String> expectedOcrData = ImmutableMap.of("first_name", "value1", "last_name", "value2", "email", "value3");
         assertThat(getOcrData(caseDetails)).isEqualTo(expectedOcrData);
 
         List<String> expectedOcrDataWarnings = Arrays.asList("warning 1", "warning 2");
