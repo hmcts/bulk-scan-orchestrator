0b9a612e0ddb6528533772ba069b490c0c9f16ce
donatas M <1420191+doncem@users.noreply.github.com>
2019-10-08 07:17:56 +0100

    Add request logging for CCD API client (#630)
    * Log all warnings for processed callback request
    
    * Add debug logging for ccd requests
    
    * Limit to specific endpoint
    
    * Fix(?) sonarqube
    
    * Add more restriction to url
    
    * Replace with config changes
    
    * Remove sonarqube amendments

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -83,6 +83,9 @@ locals {
     TRANSFORMATION_URL_BULKSCAN = "${var.transformation_url_bulkscan}"
     TRANSFORMATION_URL_PROBATE  = "${var.transformation_url_probate}"
     // endregion
+
+    // TMP
+    CCD_LOG_LEVEL = "${var.ccd-feign-logging}"
   }
 }
 
