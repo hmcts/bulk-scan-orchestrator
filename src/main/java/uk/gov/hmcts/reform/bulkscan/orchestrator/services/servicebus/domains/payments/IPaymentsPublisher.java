3f24a735a30c0de77661f0dc817148a1b0e9b72d
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-10-09 15:18:14 +0100

    Payments: Add method for sending update commands to the queue (#644)

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/domains/payments/IPaymentsPublisher.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/domains/payments/IPaymentsPublisher.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments;
 
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.model.CreatePaymentsCommand;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.model.UpdatePaymentsCommand;
 
 public interface IPaymentsPublisher {
 
     void send(CreatePaymentsCommand createPaymentsCommand);
+
+    void send(UpdatePaymentsCommand updatePaymentsCommand);
 }
