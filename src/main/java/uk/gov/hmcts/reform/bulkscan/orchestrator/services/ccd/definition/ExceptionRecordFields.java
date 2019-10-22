08b0834b259888cc0d8ca9e770c59e45bbca0187
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-10-22 10:04:15 +0100

    Feature/send payment update message after creating case from exception record (#685)

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/definition/ExceptionRecordFields.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/definition/ExceptionRecordFields.java
@@ -13,6 +13,9 @@ public final class ExceptionRecordFields {
     public static final String CASE_REFERENCE = "caseReference";
     public static final String DISPLAY_WARNINGS = "displayWarnings";
     public static final String OCR_DATA_VALIDATION_WARNINGS = "ocrDataValidationWarnings";
+    public static final String CONTAINS_PAYMENTS = "containsPayments";
+    public static final String ENVELOPE_ID = "envelopeId";
+    public static final String PO_BOX_JURISDICTION = "poBoxJurisdiction";
 
     private ExceptionRecordFields() {
         // hiding the constructor
