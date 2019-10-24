d5cc7f79e01581b1992ff47e3ac63f6f0e68fad1
apereverzin <andrey.pereverzin@hotmail.com>
2019-10-24 15:59:21 +0100

    Test

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/PaymentForExistingCaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/PaymentForExistingCaseTest.java
@@ -1,17 +1,26 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator;
 
 import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.dm.DocumentManagementUploadService;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.helper.CaseSearcher;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.helper.CcdCaseCreator;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.helper.EnvelopeMessager;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdApi;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.events.CreateExceptionRecord;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
-import java.time.Instant;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @ActiveProfiles("nosb") // no servicebus queue handler registration
@@ -26,15 +35,49 @@ class PaymentForExistingCaseTest {
     //@Autowired
     //private PaymentsPublisher paymentsPublisher;
 
+    @Autowired
+    private CaseSearcher caseSearcher;
+
+    @Autowired
+    private EnvelopeMessager envelopeMessager;
+
+    @Autowired
+    private DocumentManagementUploadService dmUploadService;
+
+    private String dmUrl;
+
+    @BeforeEach
+    void setup() {
+
+//        dmUrl = dmUploadService.uploadToDmStore(
+//            "Certificate.pdf",
+//            "documents/supplementary-evidence.pdf"
+//        );
+    }
+
     @Test
     public void should_set_awaiting_payment_false_after_payment_sent() throws Exception {
-        //given
-        CaseDetails caseDetails =
-            ccdCaseCreator.createCase(
-                emptyList(),
-                Instant.now(),
-                ImmutableMap.of("awaitingPaymentDCNProcessing", "Yes")
-            );
+        // given
+        UUID randomPoBox = UUID.randomUUID();
+
+        // when
+        String messageEnvelopeId = envelopeMessager.sendMessageFromFile(
+            "envelopes/new-envelope-with-evidence.json", // with payments dcn
+            "0000000000000000",
+            null,
+            randomPoBox,
+            dmUrl
+        );
+
+        // then
+        await("Exception record should be created")
+            .atMost(60, TimeUnit.SECONDS)
+            .pollInterval(Duration.ofSeconds(5))
+            .until(() -> findCasesByPoBox(randomPoBox).size() == 1);
+
+        CaseDetails caseDetails = findCasesByPoBox(randomPoBox).get(0);
+        assertThat(caseDetails.getCaseTypeId()).isEqualTo("BULKSCAN_ExceptionRecord");
+        assertThat(caseDetails.getJurisdiction()).isEqualTo("BULKSCAN");
         //assertThat(caseDetails.getData().get("awaitingPaymentDCNProcessing")).isEqualTo("Yes");
         //CaseDetails caseDetails = ccdCaseCreator.createCase(emptyList(), Instant.now());
 
@@ -65,5 +108,15 @@ class PaymentForExistingCaseTest {
         CaseDetails caseDetailsUpdated =
             ccdApi.getCase(Long.toString(caseDetails.getId()), caseDetails.getJurisdiction());
         return caseDetailsUpdated.getData().get("awaitingPaymentDCNProcessing").equals("No");
+    }
+
+    private List<CaseDetails> findCasesByPoBox(UUID poBox) {
+        return caseSearcher.search(
+            SampleData.JURSIDICTION,
+            SampleData.CONTAINER.toUpperCase() + "_" + CreateExceptionRecord.CASE_TYPE,
+            ImmutableMap.of(
+                "case.poBox", poBox.toString()
+            )
+        );
     }
 }
