a20fc9466784d4412f956cd397c1203a5a2b9e86
apereverzin <andrey.pereverzin@hotmail.com>
2019-10-28 14:00:52 +0000

    Introduced ExceptionRecordCreator

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/PaymentForExistingCaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/PaymentForExistingCaseTest.java
@@ -1,43 +1,36 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator;
 
 import com.google.common.collect.ImmutableMap;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.dm.DocumentManagementUploadService;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.helper.CaseSearcher;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.helper.CcdCaseCreator;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.helper.EnvelopeMessager;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.helper.ExceptionRecordCreator;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdApi;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.events.CreateExceptionRecord;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.IPaymentsPublisher;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.model.CreatePaymentsCommand;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.model.PaymentData;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import static java.util.Collections.emptyList;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.helper.CaseDataExtractor.getCaseDataForField;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.definition.YesNoFieldValues.NO;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.definition.YesNoFieldValues.YES;
 
 @SpringBootTest
 @ActiveProfiles("nosb") // no servicebus queue handler registration
 class PaymentForExistingCaseTest {
 
+    public static final String AWAITING_PAYMENT_DCN_PROCESSING = "awaitingPaymentDCNProcessing";
     @Autowired
     private CcdApi ccdApi;
 
     @Autowired
-    private CcdCaseCreator ccdCaseCreator;
+    private ExceptionRecordCreator exceptionRecordCreator;
 
     @Autowired
     private IPaymentsPublisher paymentsPublisher;
@@ -45,89 +38,14 @@ class PaymentForExistingCaseTest {
     @Autowired
     private CaseSearcher caseSearcher;
 
-    @Autowired
-    private EnvelopeMessager envelopeMessager;
-
-    @Autowired
-    private DocumentManagementUploadService dmUploadService;
-
-    private String dmUrl;
-
-    //@BeforeEach
-    //void setup() {
-    //    dmUrl = dmUploadService.uploadToDmStore(
-    //        "Certificate.pdf",
-    //        "documents/supplementary-evidence.pdf"
-    //    );
-    //}
-
     @Test
-    @Disabled
-    public void should_set_awaiting_payment_false() throws Exception {
-        // given
-        UUID randomPoBox = UUID.randomUUID();
-
-        // when
-        String messageEnvelopeId = envelopeMessager.sendMessageFromFile(
-            "envelopes/new-envelope-with-evidence-no-payments.json", // with payments dcn
-            "0000000000000000",
-            null,
-            randomPoBox,
-            dmUrl
-        );
-
-        // then
-        await("Exception record should be created")
-            .atMost(60, TimeUnit.SECONDS)
-            .pollInterval(Duration.ofSeconds(5))
-            .until(() -> findCasesByPoBox(randomPoBox).size() == 1);
-
-        CaseDetails caseDetails = findCasesByPoBox(randomPoBox).get(0);
-
-        // envelope ID from the JSON resource representing the test message
-        assertThat(caseDetails.getData().get("envelopeId")).isEqualTo(messageEnvelopeId);
-        assertThat(getCaseDataForField(caseDetails, "awaitingPaymentDCNProcessing")).isEqualTo("Yes");
-        assertThat(getCaseDataForField(caseDetails, "containsPayments")).isEqualTo("Yes");
-
-        //CaseDetails caseDetails = ccdCaseCreator.createCase(emptyList(), Instant.now());
-
-        // given
-        //assertThat(caseDetails.getData().get("awaitingPaymentDCNProcessing")).isEqualTo("Yes");
-
-        // when
-        // payment sent to payments queue
-        paymentsPublisher.send(
-            new CreatePaymentsCommand(
-                messageEnvelopeId,
-                Long.toString(caseDetails.getId()),
-                caseDetails.getJurisdiction(),
-                "bulkscan",
-                randomPoBox.toString(),
-                false,
-                Arrays.asList(new PaymentData("154565768"))
-            )
-        );
-
-        //then
-        await("Case is updated")
-            .atMost(180, TimeUnit.SECONDS)
-            .pollDelay(1, TimeUnit.SECONDS)
-            .until(() -> casePaymentStatusUpdated(caseDetails));
-    }
-
-    @Test
-    @Disabled
     public void should_set_awaiting_payment_false_after_payment_sent() throws Exception {
         // given
-        CaseDetails caseDetails = ccdCaseCreator.createCase(
-            emptyList()
-            , Instant.now()
-            , ImmutableMap.of(
-                //"poBox", "some"
-              )
+        CaseDetails caseDetails = exceptionRecordCreator.createExceptionRecord(
+            ImmutableMap.of(AWAITING_PAYMENT_DCN_PROCESSING, YES)
         );
 
-        assertThat(caseDetails.getData().get("awaitingPaymentDCNProcessing")).isEqualTo("Yes");
+        assertThat(caseDetails.getData().get(AWAITING_PAYMENT_DCN_PROCESSING)).isEqualTo(YES);
 
         // when
         // payment sent to payments queue
@@ -138,14 +56,14 @@ class PaymentForExistingCaseTest {
                 caseDetails.getJurisdiction(),
                 "bulkscan",
                 "po_box",
-                false,
-                Arrays.asList(new PaymentData("154565768"))
+                true,
+                asList(new PaymentData("154565768"))
             )
         );
 
         //then
         await("Case is updated")
-            .atMost(180, TimeUnit.SECONDS)
+            .atMost(30, TimeUnit.SECONDS)
             .pollDelay(1, TimeUnit.SECONDS)
             .until(() -> casePaymentStatusUpdated(caseDetails));
     }
@@ -153,16 +71,6 @@ class PaymentForExistingCaseTest {
     private Boolean casePaymentStatusUpdated(CaseDetails caseDetails) {
         CaseDetails caseDetailsUpdated =
             ccdApi.getCase(Long.toString(caseDetails.getId()), caseDetails.getJurisdiction());
-        return caseDetailsUpdated.getData().get("awaitingPaymentDCNProcessing").equals("No");
-    }
-
-    private List<CaseDetails> findCasesByPoBox(UUID poBox) {
-        return caseSearcher.search(
-            SampleData.JURSIDICTION,
-            SampleData.CONTAINER.toUpperCase() + "_" + CreateExceptionRecord.CASE_TYPE,
-            ImmutableMap.of(
-                "case.poBox", poBox.toString()
-            )
-        );
+        return caseDetailsUpdated.getData().get(AWAITING_PAYMENT_DCN_PROCESSING).equals(NO);
     }
 }
--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/helper/ExceptionRecordCreator.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/helper/ExceptionRecordCreator.java
@@ -3,61 +3,46 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.helper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.model.ccd.SupplementaryEvidence;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.model.ccd.mappers.SupplementaryEvidenceMapper;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdAuthenticator;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdAuthenticatorFactory;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Document;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static java.util.Collections.emptyList;
-
 @Service
-public class CcdCaseCreator {
+public class ExceptionRecordCreator {
 
-    private static final Logger log = LoggerFactory.getLogger(CcdCaseCreator.class);
+    private static final Logger log = LoggerFactory.getLogger(ExceptionRecordCreator.class);
 
     private static final String JURISDICTION = "BULKSCAN";
-    private static final String CASE_TYPE_ID = "Bulk_Scanned";
-    private static final String CREATE_CASE_EVENT_TYPE_ID = "createCase";
+    private static final String CASE_TYPE_ID = "BULKSCAN_ExceptionRecord";
+    private static final String CREATE_EXCEPTION_TYPE_ID = "createException";
 
     private final CcdAuthenticatorFactory ccdAuthenticatorFactory;
     private final CoreCaseDataApi coreCaseDataApi;
 
-    private final SupplementaryEvidenceMapper supplementaryEvidenceMapper;
-
-    public CcdCaseCreator(
+    public ExceptionRecordCreator(
         CcdAuthenticatorFactory ccdAuthenticatorFactory,
-        CoreCaseDataApi coreCaseDataApi,
-        SupplementaryEvidenceMapper supplementaryEvidenceMapper
+        CoreCaseDataApi coreCaseDataApi
     ) {
         this.ccdAuthenticatorFactory = ccdAuthenticatorFactory;
         this.coreCaseDataApi = coreCaseDataApi;
-        this.supplementaryEvidenceMapper = supplementaryEvidenceMapper;
     }
 
-    public CaseDetails createCase(List<Document> documents, Instant deliveryDate) {
-        String legacyId = "legacy-id-" + (long) (Math.random() * 100_000_000d);
-        return createCase(legacyId, documents, deliveryDate);
-    }
-
-    public CaseDetails createCase(String legacyId, List<Document> documents, Instant deliveryDate) {
+    public CaseDetails createExceptionRecord(
+        Map<String, Object> dataMap
+    ) {
         log.info("Creating new case");
         CcdAuthenticator authenticator = ccdAuthenticatorFactory.createForJurisdiction(JURISDICTION);
 
         StartEventResponse startEventResponse = startEventForCreateCase(authenticator);
         log.info("Started {} event for creating a new case", startEventResponse.getEventId());
 
-        CaseDataContent caseDataContent = prepareCaseData(startEventResponse, documents, legacyId, deliveryDate);
+        CaseDataContent caseDataContent = prepareCaseData(startEventResponse, dataMap);
 
         CaseDetails caseDetails = submitNewCase(authenticator, caseDataContent);
         log.info("Submitted {} event for creating a new case", startEventResponse.getEventId());
@@ -67,29 +52,16 @@ public class CcdCaseCreator {
 
     private CaseDataContent prepareCaseData(
         StartEventResponse startEventResponse,
-        List<Document> documents,
-        String legacyId,
-        Instant deliveryDate
+        Map<String, Object> dataMap
     ) {
-        SupplementaryEvidence supplementaryEvidence =
-            supplementaryEvidenceMapper.map(emptyList(), documents, deliveryDate);
-
-        Map<String, Object> eventData = new HashMap<>();
-        eventData.put("evidenceHandled", supplementaryEvidence.evidenceHandled);
-        eventData.put("scannedDocuments", supplementaryEvidence.scannedDocuments);
-
-        if (legacyId != null) {
-            eventData.put("legacyId", legacyId);
-        }
-
         return CaseDataContent.builder()
             .eventToken(startEventResponse.getToken())
             .event(Event.builder()
-                .id(CREATE_CASE_EVENT_TYPE_ID)
+                .id(CREATE_EXCEPTION_TYPE_ID)
                 .summary("create new case")
                 .description("create new case for tests")
                 .build())
-            .data(eventData)
+            .data(dataMap)
             .build();
     }
 
@@ -100,7 +72,7 @@ public class CcdCaseCreator {
             authenticator.getUserDetails().getId(),
             JURISDICTION,
             CASE_TYPE_ID,
-            CREATE_CASE_EVENT_TYPE_ID
+            CREATE_EXCEPTION_TYPE_ID
         );
     }
 
