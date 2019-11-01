0f419636522392994fcffb4fe52a4a92c2e39be7
aliveni <aliveni.choppa@hmcts.net>
2019-11-01 22:31:37 +0000

    add the missing CcdCaseReferenceType class (#730)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/domains/envelopes/model/CcdCaseReferenceType.java
@@ -0,0 +1,7 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model;
+
+public enum CcdCaseReferenceType {
+    CASE_REFERENCE,
+    LEGACY_CASE_REFERENCE,
+    ALL
+}
