a4cdd5f273bc1c1689fcc8bd2f01c86e39fbda4f
aliveni <aliveni.choppa@hmcts.net>
2019-11-07 11:38:56 +0000

    Add envelope case reference fields to the exception record (#720)

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/SampleData.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/SampleData.java
@@ -184,6 +184,26 @@ public class SampleData {
         );
     }
 
+    public static Envelope envelope(String caseRef, String legacyCaseRef, Classification classification) {
+        return new Envelope(
+            ENVELOPE_ID,
+            caseRef,
+            legacyCaseRef,
+            PO_BOX,
+            JURSIDICTION,
+            CONTAINER,
+            "zip-file-test.zip",
+            FORM_TYPE,
+            Instant.now(),
+            Instant.now(),
+            classification,
+            documents(1),
+            ImmutableList.of(new Payment("dcn1")),
+            ImmutableList.of(new OcrDataField("fieldName1", "value1")),
+            asList("warning 1", "warning 2")
+        );
+    }
+
     private static List<Document> documents(int numberOfDocuments) {
         return Stream.iterate(1, i -> i + 1)
             .map(index ->
