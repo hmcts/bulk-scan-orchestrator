cdf5feb09b5bb5d7f24ee4b473c3b5027d68cabe
apereverzin <andrey.pereverzin@hotmail.com>
2019-11-19 16:54:11 +0000

    Classes common to transformation and update moved out of transformation package

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/client/CaseClientServiceException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/client/CaseClientServiceException.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.bulkscan.orchestrator.client.transformation;
+package uk.gov.hmcts.reform.bulkscan.orchestrator.client;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpStatusCodeException;
