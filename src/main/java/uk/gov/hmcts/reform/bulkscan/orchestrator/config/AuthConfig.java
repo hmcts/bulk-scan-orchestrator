d7452bfeaa613d200d4a837810a85712cd8f1a6b
Donatas Martinkus <doncem@users.noreply.github.com>
2019-12-09 08:54:09 +0000

    Enable `IdamApi` feign client

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AuthConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AuthConfig.java
@@ -1,13 +1,16 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @Configuration
+@EnableFeignClients(clients = IdamApi.class)
 public class AuthConfig {
 
     @Bean
