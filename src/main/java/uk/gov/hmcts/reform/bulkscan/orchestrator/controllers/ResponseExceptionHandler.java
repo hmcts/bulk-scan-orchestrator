d88d94b17fae39259b15aa35191307250b1e7006
Rafal Kondratowicz <rkondr@gmail.com>
2019-12-10 15:49:59 +0000

    Send message in body

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/ResponseExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/ResponseExceptionHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.controllers;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,18 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(ResponseExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<Void> handleInternalException(Exception exception) {
+    protected ResponseEntity<ErrorResponse> handleInternalException(Exception exception) {
         log.error(exception.getMessage(), exception);
-        return status(INTERNAL_SERVER_ERROR).build();
+        return status(INTERNAL_SERVER_ERROR).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    public class ErrorResponse {
+
+        @JsonProperty("message")
+        public final String message;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+        }
     }
 }
