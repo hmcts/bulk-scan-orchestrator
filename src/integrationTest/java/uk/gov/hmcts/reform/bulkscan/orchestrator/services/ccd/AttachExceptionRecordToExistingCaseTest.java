bca8d70ea2e2b38de7e951be1898186457e302df
Rafal Kondratowicz <rkondr@gmail.com>
2019-12-10 16:37:03 +0000

    fix another test

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/AttachExceptionRecordToExistingCaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/AttachExceptionRecordToExistingCaseTest.java
@@ -254,8 +254,7 @@ class AttachExceptionRecordToExistingCaseTest {
             .headers(userHeaders())
             .post(CALLBACK_ATTACH_CASE_PATH)
             .then()
-            .statusCode(200)
-            .body(RESPONSE_FIELD_ERRORS, hasItem(AttachCaseCallbackService.INTERNAL_ERROR_MSG));
+            .statusCode(500);
     }
 
     @DisplayName("Should fail correctly if document is duplicate or document is already attached")
