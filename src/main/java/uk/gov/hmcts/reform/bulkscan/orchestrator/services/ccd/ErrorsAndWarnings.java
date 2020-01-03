68b708b9798e930d162ae765cf565f3bfbdb561b
apereverzin <andrey.pereverzin@hotmail.com>
2020-01-03 14:00:50 +0000

    Changed access level

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/ErrorsAndWarnings.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/ErrorsAndWarnings.java
@@ -8,16 +8,16 @@ public class ErrorsAndWarnings {
     private final List<String> errors;
     private final List<String> warnings;
 
-    public ErrorsAndWarnings(List<String> errors, List<String> warnings) {
+    private ErrorsAndWarnings(List<String> errors, List<String> warnings) {
         this.errors = errors;
         this.warnings = warnings;
     }
 
-    public static ErrorsAndWarnings withErrors(List<String> errors) {
+    static ErrorsAndWarnings withErrors(List<String> errors) {
         return new ErrorsAndWarnings(errors, emptyList());
     }
 
-    public static ErrorsAndWarnings withWarnings(List<String> warnings) {
+    static ErrorsAndWarnings withWarnings(List<String> warnings) {
         return new ErrorsAndWarnings(emptyList(), warnings);
     }
 
