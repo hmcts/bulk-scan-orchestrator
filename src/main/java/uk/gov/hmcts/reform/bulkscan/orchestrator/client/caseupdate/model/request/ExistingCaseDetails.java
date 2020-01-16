21a97e0431fb80b8740d568f5d0ba8f6a712fd4a
mdayican <mustafa.dayican@hmcts.net>
2020-01-16 15:54:40 +0000

    add case id to update

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/client/caseupdate/model/request/ExistingCaseDetails.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/client/caseupdate/model/request/ExistingCaseDetails.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 
 public class ExistingCaseDetails {
+
+    @JsonProperty("id")
+    public final String id;
+
     @JsonProperty("case_type_id")
     public final String caseTypeId;
 
@@ -12,9 +16,11 @@ public class ExistingCaseDetails {
     public final Map<String, Object> data;
 
     public ExistingCaseDetails(
+        String id,
         String caseTypeId,
         Map<String, Object> data
     ) {
+        this.id = id;
         this.caseTypeId = caseTypeId;
         this.data = data;
     }
