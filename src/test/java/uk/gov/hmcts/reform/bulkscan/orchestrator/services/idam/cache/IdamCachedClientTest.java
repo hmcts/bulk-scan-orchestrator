2023474bad2b03a5ac7d8e175a77a6df1c54933b
mdayican <mustafa.dayican@hmcts.net>
2020-04-28 00:03:41 +0100

    use cache write instead of removal listener

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/idam/cache/IdamCachedClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/idam/cache/IdamCachedClientTest.java
@@ -250,7 +250,6 @@ class IdamCachedClientTest {
         assertThat(userDetailsBefore).isEqualTo(expectedUserDetails1);
 
         idamCachedClient.removeAccessTokenFromCache(jurisdiction1);
-        idamCachedClient.cleanUpAccessTokenCache();
 
         UserDetails userDetailsAfterInvalidating = idamCachedClient.getUserDetails(token1);
         assertThat(userDetailsAfterInvalidating).usingRecursiveComparison().isEqualTo(expectedUserDetails2);
