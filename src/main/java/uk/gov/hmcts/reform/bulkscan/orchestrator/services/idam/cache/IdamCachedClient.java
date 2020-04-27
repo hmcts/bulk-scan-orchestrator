b82c2b051d6f4e1acbc15a94446a2c102a5f8575
mdayican <mustafa.dayican@hmcts.net>
2020-04-27 17:06:39 +0100

    add userdetail cache (#943)

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/idam/cache/IdamCachedClient.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/idam/cache/IdamCachedClient.java
@@ -5,19 +5,25 @@ import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.Credential;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.JurisdictionToUserMapping;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 @Service
 public class IdamCachedClient {
 
+    private static final Logger log = LoggerFactory.getLogger(IdamCachedClient.class);
     public static final String BEARER_AUTH_TYPE = "Bearer ";
     public static final String EXPIRES_IN = "expires_in";
 
     private static Cache<String, CachedIdamToken> accessTokenCache;
+    private static Cache<String, UserDetails> userDetailsCache;
 
     private final IdamClient idamClient;
     private final JurisdictionToUserMapping users;
@@ -31,20 +37,32 @@ public class IdamCachedClient {
         this.users = users;
         this.accessTokenCache = Caffeine.newBuilder()
             .expireAfter(accessTokenCacheExpiry)
+            .removalListener(this::onCachedIdamTokenRemoval)
+            .build();
+
+        this.userDetailsCache =  Caffeine.newBuilder()
+            .maximumSize(200)
             .build();
     }
 
     public String getAccessToken(String jurisdiction) {
+        log.info("Get access token for jurisdiction: {} ", jurisdiction);
         CachedIdamToken cachedIdamToken = this.accessTokenCache
             .get(jurisdiction, j -> retrieveToken(j));
         return cachedIdamToken.accessToken;
     }
 
     public void removeAccessTokenFromCache(String jurisdiction) {
+        log.info("Remove access token from cache for jurisdiction: {} ", jurisdiction);
         accessTokenCache.invalidate(jurisdiction);
     }
 
+    public void cleanUpAccessTokenCache() {
+        accessTokenCache.cleanUp();
+    }
+
     private CachedIdamToken retrieveToken(String jurisdiction) {
+        log.info("Retrieve access token for jurisdiction: {} from IDAM", jurisdiction);
         Credential user = users.getUser(jurisdiction);
         String tokenWithBearer = idamClient.authenticateUser(
             user.getUsername(),
@@ -68,6 +86,28 @@ public class IdamCachedClient {
         }
 
         return expires.asLong();
+    }
+
+    private void onCachedIdamTokenRemoval(
+        String jurisdiction,
+        CachedIdamToken cachedIdamToken,
+        RemovalCause cause
+    ) {
+        log.info("On access token removal invalidate user details. "
+                + "Access token removed for jurisdiction: {}, cause: {} ",
+            jurisdiction,
+            cause);
+        userDetailsCache.invalidate(cachedIdamToken.accessToken);
+    }
+
+    public UserDetails getUserDetails(String accessToken) {
+        log.info("Get user details");
+        return this.userDetailsCache.get(accessToken, this::retrieveUserDetails);
+    }
+
+    private UserDetails retrieveUserDetails(String accessToken) {
+        log.info("Retrieve user details from IDAM");
+        return idamClient.getUserDetails(accessToken);
     }
 
 }
