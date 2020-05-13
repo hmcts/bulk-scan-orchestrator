a8344d78df9409c8c0b33184196370acc25ab519
Donatas Martinkus <doncem@users.noreply.github.com>
2020-05-13 12:23:30 +0100

    Add new method for retrieving docs to attach

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/DocumentsTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/DocumentsTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.junit.jupiter.api.DisplayName;
@@ -18,13 +19,104 @@ import javax.validation.constraints.NotNull;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
 import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.Documents.getScannedDocuments;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.Documents.removeAlreadyAttachedDocuments;
 
 class DocumentsTest {
     private static final String SCANNED_DOCUMENTS = "scannedDocuments";
     private static final String DOCUMENT_NUMBER = "id";
+
+    @Test
+    void should_remove_all_documents_from_exception_record_because_all_of_them_are_already_in_target_case() {
+        // given
+        String exceptionRecordReference = "REF";
+        List<Map<String, Object>> exceptionRecordDocuments = singletonList(
+            singletonMap("value", singletonMap("controlNumber", "e-123"))
+        );
+        List<Map<String, Object>> targetCaseDocuments = singletonList(
+            singletonMap("value", ImmutableMap.of(
+                "controlNumber", "e-123",
+                "exceptionRecordReference", exceptionRecordReference
+            ))
+        );
+
+        // when
+        List<Map<String, Object>> actualDocumentsToAttach = removeAlreadyAttachedDocuments(
+            exceptionRecordDocuments,
+            targetCaseDocuments,
+            exceptionRecordReference
+        );
+
+        // then
+        assertThat(actualDocumentsToAttach).asList().isEmpty();
+    }
+
+    @Test
+    void should_remove_overlapping_documents_from_exception_record_because_they_are_already_in_target_case() {
+        // given
+        String exceptionRecordReference = "REF";
+        List<Map<String, Object>> exceptionRecordDocuments = ImmutableList.of(
+            singletonMap("value", singletonMap("controlNumber", "e-123")),
+            singletonMap("value", singletonMap("controlNumber", "e-321"))
+        );
+        List<Map<String, Object>> targetCaseDocuments = singletonList(
+            singletonMap("value", ImmutableMap.of(
+                "controlNumber", "e-123",
+                "exceptionRecordReference", exceptionRecordReference
+            ))
+        );
+
+        // when
+        List<Map<String, Object>> actualDocumentsToAttach = removeAlreadyAttachedDocuments(
+            exceptionRecordDocuments,
+            targetCaseDocuments,
+            exceptionRecordReference
+        );
+
+        // then
+        assertThat(actualDocumentsToAttach)
+            .asList()
+            .hasSize(1)
+            .first()
+            .isInstanceOfSatisfying(actualDocumentsToAttach.get(0).getClass(), document ->
+                assertThat(document)
+                    .hasFieldOrProperty("value")
+                    .extracting("value")
+                    .isInstanceOfSatisfying(document.getClass(), actualDocument ->
+                        assertThat(actualDocument)
+                            .hasFieldOrPropertyWithValue("controlNumber", "e-321")
+                    )
+            );
+    }
+
+    @Test
+    void should_leave_all_documents_in_exception_record_because_there_is_nothing_matching_in_target_case() {
+        // given
+        List<Map<String, Object>> exceptionRecordDocuments = ImmutableList.of(
+            singletonMap("value", singletonMap("controlNumber", "e-123")),
+            singletonMap("value", singletonMap("controlNumber", "e-321"))
+        );
+        List<Map<String, Object>> targetCaseDocuments = singletonList(
+            singletonMap("value", ImmutableMap.of(
+                "controlNumber", "e-123",
+                "exceptionRecordReference", "REF-1"
+            ))
+        );
+
+        // when
+        List<Map<String, Object>> actualDocumentsToAttach = removeAlreadyAttachedDocuments(
+            exceptionRecordDocuments,
+            targetCaseDocuments,
+            "REF-2"
+        );
+
+        // then
+        assertThat(actualDocumentsToAttach).asList().hasSize(2);
+    }
 
     @Test
     void should_return_document_control_number_when_one_is_present() {
