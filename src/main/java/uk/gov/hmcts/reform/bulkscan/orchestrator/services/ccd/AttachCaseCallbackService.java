cd64ef6c1df722fa4ffa170ab798d1d961e33d95
Donatas Martinkus <doncem@users.noreply.github.com>
2020-07-10 11:55:12 +0100

    Extract private class

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/AttachCaseCallbackService.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/AttachCaseCallbackService.java
@@ -540,46 +540,6 @@ public class AttachCaseCallbackService {
             && exceptionRecord.getData().get(SEARCH_CASE_REFERENCE_TYPE) != null;
     }
 
-    /**
-     * Information received in the callback call for the attach event.
-     */
-    private static class AttachToCaseEventData {
-        public final String exceptionRecordJurisdiction;
-        public final String service;
-        public final String targetCaseRef;
-        public final String targetCaseRefType;
-        public final Long exceptionRecordId;
-        public final List<Map<String, Object>> exceptionRecordDocuments;
-        public final String idamToken;
-        public final String userId;
-        public final Classification classification;
-        public final ExceptionRecord exceptionRecord;
-
-        public AttachToCaseEventData(
-            String exceptionRecordJurisdiction,
-            String service,
-            String targetCaseRefType,
-            String targetCaseRef,
-            Long exceptionRecordId,
-            List<Map<String, Object>> exceptionRecordDocuments,
-            String idamToken,
-            String userId,
-            Classification classification,
-            ExceptionRecord exceptionRecord
-        ) {
-            this.exceptionRecordJurisdiction = exceptionRecordJurisdiction;
-            this.service = service;
-            this.targetCaseRefType = targetCaseRefType;
-            this.targetCaseRef = targetCaseRef;
-            this.exceptionRecordId = exceptionRecordId;
-            this.exceptionRecordDocuments = exceptionRecordDocuments;
-            this.idamToken = idamToken;
-            this.userId = userId;
-            this.classification = classification;
-            this.exceptionRecord = exceptionRecord;
-        }
-    }
-
     private Map<String, Object> mergeCaseFields(
         Map<String, Object> originalFields,
         Map<String, Object> modifiedFields
