1d08a3ca92e8144ba6d51299c1c777b5378b2f7b
Rafal Kondratowicz <rkondr@gmail.com>
2020-09-09 17:38:39 +0200

    Extract new application handler

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/EnvelopeHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/EnvelopeHandlerTest.java
@@ -8,31 +8,21 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CaseFinder;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.PaymentsProcessor;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.casecreation.AutoCaseCreator;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.casecreation.CaseCreationException;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Envelope;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeCcdAction;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeProcessingResult;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.CASE_ID;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.CASE_REF;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.JURSIDICTION;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.envelope;
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.casecreation.CaseCreationResult.caseCreated;
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.casecreation.CaseCreationResult.potentiallyRecoverableFailure;
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.casecreation.CaseCreationResult.unrecoverableFailure;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.EXCEPTION;
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.NEW_APPLICATION;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.SUPPLEMENTARY_EVIDENCE;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.SUPPLEMENTARY_EVIDENCE_WITH_OCR;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeCcdAction.AUTO_ATTACHED_TO_CASE;
@@ -41,14 +31,12 @@ import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.doma
 @ExtendWith(MockitoExtension.class)
 class EnvelopeHandlerTest {
 
-    private static final int MAX_ALLOWED_RETRIES_FOR_RECOVERABLE_FAILURES = 2;
-
     @Mock private AttachDocsToSupplementaryEvidence attachDocsToSupplementaryEvidence;
     @Mock private CreateExceptionRecord createExceptionRecord;
     @Mock private CaseFinder caseFinder;
     @Mock private CaseDetails caseDetails;
     @Mock private PaymentsProcessor paymentsProcessor;
-    @Mock private AutoCaseCreator autoCaseCreator;
+    @Mock private NewApplicationHandler newApplicationHandler;
 
     private EnvelopeHandler envelopeHandler;
 
@@ -59,7 +47,7 @@ class EnvelopeHandlerTest {
             createExceptionRecord,
             caseFinder,
             paymentsProcessor,
-            autoCaseCreator
+            newApplicationHandler
         );
     }
 
@@ -125,78 +113,6 @@ class EnvelopeHandlerTest {
         assertThat(CASE_ID).isEqualTo(envelopeProcessingResult.ccdId);
 
         verify(createExceptionRecord).tryCreateFrom(envelope);
-        verify(paymentsProcessor).createPayments(envelope, CASE_ID, true);
-    }
-
-    @Test
-    void should_not_create_exception_record_for_new_application_when_case_creation_successful() {
-        // given
-        long caseId = 1234L;
-        Envelope envelope = envelope(NEW_APPLICATION, JURSIDICTION, CASE_REF);
-        given(autoCaseCreator.createCase(envelope)).willReturn(caseCreated(caseId));
-
-        // when
-        EnvelopeProcessingResult envelopeProcessingResult = envelopeHandler.handleEnvelope(envelope, 0);
-
-        // then
-        assertThat(envelopeProcessingResult.envelopeCcdAction).isEqualTo(EnvelopeCcdAction.CASE_CREATED);
-        assertThat(envelopeProcessingResult.ccdId).isEqualTo(caseId);
-
-        verify(autoCaseCreator).createCase(envelope);
-        verify(createExceptionRecord, never()).tryCreateFrom(any());
-        verify(paymentsProcessor).createPayments(envelope, caseId, false);
-    }
-
-    @Test
-    void should_create_exception_record_for_new_application_when_unrecoverable_failure() {
-        // given
-        Envelope envelope = envelope(NEW_APPLICATION, JURSIDICTION, CASE_REF);
-        given(autoCaseCreator.createCase(envelope)).willReturn(unrecoverableFailure());
-        given(createExceptionRecord.tryCreateFrom(envelope)).willReturn(CASE_ID);
-
-        // when
-        EnvelopeProcessingResult envelopeProcessingResult = envelopeHandler.handleEnvelope(envelope, 0);
-
-        // then
-        assertThat(EXCEPTION_RECORD).isEqualTo(envelopeProcessingResult.envelopeCcdAction);
-        assertThat(CASE_ID).isEqualTo(envelopeProcessingResult.ccdId);
-
-        verify(autoCaseCreator).createCase(envelope);
-        verify(createExceptionRecord).tryCreateFrom(envelope);
-        verify(caseFinder, never()).findCase(any());
-        verify(paymentsProcessor).createPayments(envelope, CASE_ID, true);
-    }
-
-    @Test
-    void should_throw_exception_record_for_new_application_when_recoverable_failure() {
-        Envelope envelope = envelope(NEW_APPLICATION, JURSIDICTION, CASE_REF);
-        given(autoCaseCreator.createCase(envelope)).willReturn(potentiallyRecoverableFailure());
-
-        assertThatThrownBy(() -> envelopeHandler.handleEnvelope(envelope, 0))
-            .isInstanceOf(CaseCreationException.class)
-            .hasMessage("Case creation failed due to a potentially recoverable error");
-
-        verify(autoCaseCreator).createCase(envelope);
-    }
-
-    @Test
-    void should_create_exception_record_for_new_application_when_too_many_deliveries() {
-        // given
-        Envelope envelope = envelope(NEW_APPLICATION, JURSIDICTION, CASE_REF);
-        given(autoCaseCreator.createCase(envelope)).willReturn(potentiallyRecoverableFailure());
-        given(createExceptionRecord.tryCreateFrom(envelope)).willReturn(CASE_ID);
-
-        // when
-        EnvelopeProcessingResult envelopeProcessingResult =
-            envelopeHandler.handleEnvelope(envelope, MAX_ALLOWED_RETRIES_FOR_RECOVERABLE_FAILURES);
-
-        // then
-        assertThat(EXCEPTION_RECORD).isEqualTo(envelopeProcessingResult.envelopeCcdAction);
-        assertThat(CASE_ID).isEqualTo(envelopeProcessingResult.ccdId);
-
-        verify(autoCaseCreator).createCase(envelope);
-        verify(createExceptionRecord).tryCreateFrom(envelope);
-        verify(caseFinder, never()).findCase(any());
         verify(paymentsProcessor).createPayments(envelope, CASE_ID, true);
     }
 
