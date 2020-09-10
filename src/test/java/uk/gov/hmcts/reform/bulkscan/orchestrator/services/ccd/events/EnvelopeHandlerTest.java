3620b63de7c85563338863cc99058aa9138cff84
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2020-09-10 17:23:01 +0200

    Extract supplementary evidence handler (#1208)

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/EnvelopeHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/EnvelopeHandlerTest.java
@@ -10,9 +10,6 @@ import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CaseFinder;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.PaymentsProcessor;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Envelope;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeProcessingResult;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -23,9 +20,7 @@ import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.CASE_REF;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.JURSIDICTION;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.envelope;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.EXCEPTION;
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.SUPPLEMENTARY_EVIDENCE;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.SUPPLEMENTARY_EVIDENCE_WITH_OCR;
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeCcdAction.AUTO_ATTACHED_TO_CASE;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeCcdAction.EXCEPTION_RECORD;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,19 +29,18 @@ class EnvelopeHandlerTest {
     @Mock private AttachDocsToSupplementaryEvidence attachDocsToSupplementaryEvidence;
     @Mock private CreateExceptionRecord createExceptionRecord;
     @Mock private CaseFinder caseFinder;
-    @Mock private CaseDetails caseDetails;
     @Mock private PaymentsProcessor paymentsProcessor;
     @Mock private NewApplicationHandler newApplicationHandler;
+    @Mock private SupplementaryEvidenceHandler supplementaryEvidenceHandler;
 
     private EnvelopeHandler envelopeHandler;
 
     @BeforeEach
     void setUp() {
         envelopeHandler = new EnvelopeHandler(
-            attachDocsToSupplementaryEvidence,
             createExceptionRecord,
-            caseFinder,
             paymentsProcessor,
+            supplementaryEvidenceHandler,
             newApplicationHandler
         );
     }
@@ -59,44 +53,6 @@ class EnvelopeHandlerTest {
             caseFinder,
             paymentsProcessor
         );
-    }
-
-    @Test
-    void should_call_AttachDocsToSupplementaryEvidence_for_supplementary_evidence_classification_when_case_exists() {
-        // given
-        Envelope envelope = envelope(SUPPLEMENTARY_EVIDENCE, JURSIDICTION, CASE_REF);
-        given(caseFinder.findCase(envelope)).willReturn(Optional.of(caseDetails));
-        given(attachDocsToSupplementaryEvidence.attach(envelope, caseDetails)).willReturn(true);
-        Long ccdId = 321394383L;
-        given(caseDetails.getId()).willReturn(ccdId);
-
-        // when
-        EnvelopeProcessingResult envelopeProcessingResult = envelopeHandler.handleEnvelope(envelope, 0);
-
-        // then
-        assertThat(AUTO_ATTACHED_TO_CASE).isEqualTo(envelopeProcessingResult.envelopeCcdAction);
-        assertThat(ccdId).isEqualTo(envelopeProcessingResult.ccdId);
-
-        verify(attachDocsToSupplementaryEvidence).attach(envelope, caseDetails);
-        verify(paymentsProcessor).createPayments(envelope, caseDetails.getId(), false);
-    }
-
-    @Test
-    void should_call_CreateExceptionRecord_for_supplementary_evidence_classification_when_case_does_not_exist() {
-        // given
-        Envelope envelope = envelope(SUPPLEMENTARY_EVIDENCE, JURSIDICTION, CASE_REF);
-        given(caseFinder.findCase(envelope)).willReturn(Optional.empty()); // case not found
-        given(createExceptionRecord.tryCreateFrom(envelope)).willReturn(CASE_ID);
-
-        // when
-        EnvelopeProcessingResult envelopeProcessingResult = envelopeHandler.handleEnvelope(envelope, 0);
-
-        // then
-        assertThat(EXCEPTION_RECORD).isEqualTo(envelopeProcessingResult.envelopeCcdAction);
-        assertThat(CASE_ID).isEqualTo(envelopeProcessingResult.ccdId);
-
-        verify(createExceptionRecord).tryCreateFrom(envelope);
-        verify(paymentsProcessor).createPayments(envelope, CASE_ID, true);
     }
 
     @Test
@@ -129,26 +85,6 @@ class EnvelopeHandlerTest {
         assertThat(EXCEPTION_RECORD).isEqualTo(envelopeProcessingResult.envelopeCcdAction);
         assertThat(CASE_ID).isEqualTo(envelopeProcessingResult.ccdId);
 
-        verify(createExceptionRecord).tryCreateFrom(envelope);
-        verify(paymentsProcessor).createPayments(envelope, CASE_ID, true);
-    }
-
-    @Test
-    void should_call_CreateExceptionRecord_when_documents_attachment_fails_for_supplementary_evidence() {
-        // given
-        Envelope envelope = envelope(SUPPLEMENTARY_EVIDENCE, JURSIDICTION, CASE_REF);
-        given(caseFinder.findCase(envelope)).willReturn(Optional.of(caseDetails));
-        given(attachDocsToSupplementaryEvidence.attach(envelope, caseDetails)).willReturn(false);
-        given(createExceptionRecord.tryCreateFrom(envelope)).willReturn(CASE_ID);
-
-        // when
-        EnvelopeProcessingResult envelopeProcessingResult = envelopeHandler.handleEnvelope(envelope, 0);
-
-        // then
-        assertThat(EXCEPTION_RECORD).isEqualTo(envelopeProcessingResult.envelopeCcdAction);
-        assertThat(CASE_ID).isEqualTo(envelopeProcessingResult.ccdId);
-
-        verify(attachDocsToSupplementaryEvidence).attach(envelope, caseDetails);
         verify(createExceptionRecord).tryCreateFrom(envelope);
         verify(paymentsProcessor).createPayments(envelope, CASE_ID, true);
     }
