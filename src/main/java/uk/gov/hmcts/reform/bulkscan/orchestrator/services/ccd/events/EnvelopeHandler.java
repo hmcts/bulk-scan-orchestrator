78682b5f2346abe8d2efe413df9b442a9d4f31f0
Rafal Kondratowicz <rkondr@gmail.com>
2020-09-11 09:56:43 +0200

    Extract exception classification handler

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/EnvelopeHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/EnvelopeHandler.java
@@ -1,30 +1,24 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.events;
 
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.PaymentsProcessor;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Envelope;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeProcessingResult;
-
-import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeCcdAction.EXCEPTION_RECORD;
 
 @Service
 public class EnvelopeHandler {
 
-    private final CreateExceptionRecord exceptionRecordCreator;
-    private final PaymentsProcessor paymentsProcessor;
+    private final ExceptionClassificationHandler exceptionClassificationHandler;
     private final SupplementaryEvidenceWithOcrHandler supplementaryEvidenceWithOcrHandler;
     private final NewApplicationHandler newApplicationHandler;
     private final SupplementaryEvidenceHandler supplementaryEvidenceHandler;
 
     public EnvelopeHandler(
-        CreateExceptionRecord exceptionRecordCreator,
-        PaymentsProcessor paymentsProcessor,
+        ExceptionClassificationHandler exceptionClassificationHandler,
         SupplementaryEvidenceWithOcrHandler supplementaryEvidenceWithOcrHandler,
         NewApplicationHandler newApplicationHandler,
         SupplementaryEvidenceHandler supplementaryEvidenceHandler
     ) {
-        this.exceptionRecordCreator = exceptionRecordCreator;
-        this.paymentsProcessor = paymentsProcessor;
+        this.exceptionClassificationHandler = exceptionClassificationHandler;
         this.supplementaryEvidenceWithOcrHandler = supplementaryEvidenceWithOcrHandler;
         this.newApplicationHandler = newApplicationHandler;
         this.supplementaryEvidenceHandler = supplementaryEvidenceHandler;
@@ -37,7 +31,7 @@ public class EnvelopeHandler {
             case SUPPLEMENTARY_EVIDENCE_WITH_OCR:
                 return supplementaryEvidenceWithOcrHandler.handle(envelope);
             case EXCEPTION:
-                return new EnvelopeProcessingResult(createExceptionRecord(envelope), EXCEPTION_RECORD);
+                return exceptionClassificationHandler.handle(envelope);
             case NEW_APPLICATION:
                 return newApplicationHandler.handle(envelope, deliveryCount);
             default:
@@ -45,12 +39,5 @@ public class EnvelopeHandler {
                     "Cannot determine CCD action for envelope - unknown classification: " + envelope.classification
                 );
         }
-    }
-
-    private Long createExceptionRecord(Envelope envelope) {
-        Long ccdId = exceptionRecordCreator.tryCreateFrom(envelope);
-
-        paymentsProcessor.createPayments(envelope, ccdId, true);
-        return ccdId;
     }
 }
---
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/events/ExceptionClassificationHandlerTest.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.events;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.PaymentsProcessor;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Envelope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.CASE_ID;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.CASE_REF;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.JURSIDICTION;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData.envelope;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.EXCEPTION;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification.NEW_APPLICATION;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeCcdAction.EXCEPTION_RECORD;
+
+@ExtendWith(MockitoExtension.class)
+class ExceptionClassificationHandlerTest {
+
+    @Mock CreateExceptionRecord exceptionRecordCreator;
+    @Mock PaymentsProcessor paymentsProcessor;
+
+    ExceptionClassificationHandler handler;
+
+    @BeforeEach
+    void setUp() {
+        handler = new ExceptionClassificationHandler(exceptionRecordCreator, paymentsProcessor);
+    }
+
+    @Test
+    void should_create_exception_record() {
+        // given
+        Envelope envelope = envelope(EXCEPTION, JURSIDICTION, CASE_REF);
+        given(exceptionRecordCreator.tryCreateFrom(envelope)).willReturn(CASE_ID);
+
+        // when
+        var result = handler.handle(envelope);
+
+        // then
+        assertThat(result.envelopeCcdAction).isEqualTo(EXCEPTION_RECORD);
+        assertThat(result.ccdId).isEqualTo(CASE_ID);
+
+        verify(exceptionRecordCreator).tryCreateFrom(envelope);
+        verify(paymentsProcessor).createPayments(envelope, CASE_ID, true);
+    }
+
+    @Test
+    void should_throw_exception_if_envelope_is_not_of_exception_classification() {
+        // given
+        Envelope envelope = envelope(NEW_APPLICATION, JURSIDICTION, CASE_REF);
+
+        // when
+        var exc = catchThrowable(() -> handler.handle(envelope));
+
+        // then
+        assertThat(exc)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("Envelope classification");
+    }
+}
