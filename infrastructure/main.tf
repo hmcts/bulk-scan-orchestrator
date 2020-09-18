dfb10550c079fab5cf0a4eee71af80552086efbc
Donatas Martinkus <doncem@users.noreply.github.com>
2020-09-18 11:59:29 +0300

    Use HCL2 expressions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 # Make sure the resource group exists
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location_app}"
+  location = var.location_app
 }
 
 data "azurerm_key_vault" "key_vault" {
@@ -21,12 +21,12 @@ data "azurerm_key_vault" "s2s_key_vault" {
 # Copy orchestrator s2s secret from s2s key vault to bulkscan key vault
 
 data "azurerm_key_vault_secret" "s2s_secret" {
-  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
-  name      = "microservicekey-bulk-scan-orchestrator"
+  key_vault_id = data.azurerm_key_vault.s2s_key_vault.id
+  name         = "microservicekey-bulk-scan-orchestrator"
 }
 
 resource "azurerm_key_vault_secret" "bulk_scan_orchestrator_app_s2s_secret" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
   name         = "s2s-secret-bulk-scan-orchestrator"
-  value        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  value        = data.azurerm_key_vault_secret.s2s_secret.value
 }
