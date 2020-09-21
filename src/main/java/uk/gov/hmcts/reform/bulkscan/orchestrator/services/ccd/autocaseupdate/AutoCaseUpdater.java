12e6e6df7ef7e86378703b5f2142cbefec7cf37c
Rafal Kondratowicz <rkondr@gmail.com>
2020-09-21 20:28:50 +0200

    update

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/autocaseupdate/AutoCaseUpdater.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/autocaseupdate/AutoCaseUpdater.java
@@ -3,13 +3,17 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.autocaseupdate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.caseupdatedetails.CaseUpdateDetailsService;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdApi;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.EventIds;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Envelope;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
 
 import java.util.List;
+import java.util.Map;
 
+import static java.lang.String.format;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.autocaseupdate.AutoCaseUpdateResult.ABANDONED;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.autocaseupdate.AutoCaseUpdateResult.OK;
 
@@ -18,16 +22,13 @@ public class AutoCaseUpdater {
 
     private static final Logger log = LoggerFactory.getLogger(AutoCaseUpdater.class);
 
-    private final AuthTokenGenerator s2sTokenGenerator;
     private final CaseUpdateDetailsService caseUpdateDataService;
     private final CcdApi ccdApi;
 
     public AutoCaseUpdater(
-        AuthTokenGenerator s2sTokenGenerator,
         CaseUpdateDetailsService caseUpdateDataService,
         CcdApi ccdApi
     ) {
-        this.s2sTokenGenerator = s2sTokenGenerator;
         this.caseUpdateDataService = caseUpdateDataService;
         this.ccdApi = ccdApi;
     }
@@ -37,7 +38,8 @@ public class AutoCaseUpdater {
 
         switch (matchingCases.size()) {
             case 1:
-                return handleSingleMatchingCase(matchingCases.get(0), envelope);
+                String caseId = String.valueOf(matchingCases.get(0));
+                return handleSingleMatchingCase(caseId, envelope);
             case 0:
                 return handleNoMatchingCases(envelope);
             default:
@@ -45,19 +47,38 @@ public class AutoCaseUpdater {
         }
     }
 
-    private AutoCaseUpdateResult handleSingleMatchingCase(Long caseId, Envelope envelope) {
-        var existingCase = ccdApi.getCase(String.valueOf(caseId), envelope.jurisdiction);
+    private AutoCaseUpdateResult handleSingleMatchingCase(String caseId, Envelope envelope) {
+        var existingCase = ccdApi.getCase(caseId, envelope.jurisdiction);
 
-        var caseUpdateResult = caseUpdateDataService.getCaseUpdateData(envelope.container, existingCase, envelope);
+        var caseUpdateResult =
+            caseUpdateDataService
+                .getCaseUpdateData(
+                    envelope.container,
+                    existingCase,
+                    envelope
+                );
 
-        // TODO: update case
+        ccdApi.updateCase(
+            envelope.container,
+            existingCase.getCaseTypeId(),
+            EventIds.ATTACH_SCANNED_DOCS_WITH_OCR,
+            caseId,
+            startEventResponse ->
+                getCaseDataContent(
+                    caseUpdateResult.caseDetails.caseData,
+                    envelope.id,
+                    startEventResponse.getEventId(),
+                    startEventResponse.getToken()
+                ),
+            getLoggingContext(envelope)
+        );
+
         return OK;
     }
 
     private AutoCaseUpdateResult handleNoMatchingCases(Envelope envelope) {
         log.warn(
-            "Auto case update abandoned - no case found for envelope. "
-                + "Envelope ID: {}, case ref: {}",
+            "Auto case update abandoned - no case found for envelope. Envelope ID: {}, case ref: {}",
             envelope.id,
             envelope.caseRef
         );
@@ -73,5 +94,37 @@ public class AutoCaseUpdater {
             matchingCases
         );
         return ABANDONED;
+    }
+
+    private CaseDataContent getCaseDataContent(
+        Map<String, Object> caseData,
+        String envelopeId,
+        String eventId,
+        String eventToken
+    ) {
+        // TODO: update casedata with envelope ID
+
+        return CaseDataContent
+            .builder()
+            .data(caseData)
+            .event(
+                Event
+                    .builder()
+                    .id(eventId)
+                    .summary("Case automatically updated with envelope")
+                    .description("Case update with envelope " + envelopeId)
+                    .build()
+            )
+            .eventToken(eventToken)
+            .build();
+    }
+
+    private String getLoggingContext(Envelope envelope) {
+        return format(
+            "Envelope ID: %s. File name: %s. Service: %s.",
+            envelope.id,
+            envelope.zipFileName,
+            envelope.container
+        );
     }
 }
