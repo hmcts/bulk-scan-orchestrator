37c1d0152863b31da4406639af8a55ec014d0eb7
akomisarek <Adam.Komisarek@hmcts.net>
2020-09-22 16:24:28 +0100

    chore: clean up AES app before upgrading to arm2 (#1234)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,18 +9,11 @@ resource "azurerm_resource_group" "rg" {
 }
 
 locals {
-  ase_name            = "core-compute-${var.env}"
   is_preview          = "${(var.env == "preview" || var.env == "spreview")}"
   previewVaultName    = "${var.raw_product}-aat"
   nonPreviewVaultName = "${var.raw_product}-${var.env}"
   vaultName           = "${local.is_preview ? local.previewVaultName : local.nonPreviewVaultName}"
   local_env           = "${local.is_preview ? "aat" : var.env}"
-  local_ase           = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
-
-  sku_size = "${var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"}"
-
-  ccdApiUrl = "http://ccd-data-store-api-${local.local_env}.service.${local.local_ase}.internal"
-  dm_store_api_url = "http://dm-store-${local.local_env}.service.${local.local_ase}.internal"
 
   users = {
     // configures a user for a jurisdiction
@@ -42,62 +35,7 @@ locals {
 
   users_secret_names = "${values(local.supported_users)}"
 
-  users_usernames_settings = "${zipmap(
-                                    formatlist("IDAM_USERS_%s_USERNAME", keys(local.supported_users)),
-                                    data.azurerm_key_vault_secret.idam_users_usernames.*.value
-                                )}"
-
-  users_passwords_settings = "${zipmap(
-                                    formatlist("IDAM_USERS_%s_PASSWORD", keys(local.supported_users)),
-                                    data.azurerm_key_vault_secret.idam_users_passwords.*.value
-                                )}"
-
   s2s_rg  = "rpe-service-auth-provider-${local.local_env}"
-  s2s_url = "http://${local.s2s_rg}.service.core-compute-${local.local_env}.internal"
-
-  core_app_settings = {
-    S2S_URL     = "${local.s2s_url}"
-    S2S_NAME    = "${var.s2s_name}"
-    S2S_SECRET  = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-
-    IDAM_API_URL              = "${var.idam_api_url}"
-    IDAM_CLIENT_SECRET        = "${data.azurerm_key_vault_secret.idam_client_secret.value}"
-    IDAM_CLIENT_REDIRECT_URI  = "${var.idam_client_redirect_uri}"
-    CORE_CASE_DATA_API_URL    = "${local.ccdApiUrl}"
-    DOCUMENT_MANAGEMENT_URL   = "${local.dm_store_api_url}"
-
-    DELETE_ENVELOPES_DLQ_MESSAGES_ENABLED = "${var.delete_envelopes_dlq_messages_enabled}"
-    DELETE_ENVELOPES_DLQ_MESSAGES_CRON    = "${var.delete_envelopes_dlq_messages_cron}"
-    DELETE_ENVELOPES_DLQ_MESSAGES_TTL     = "${var.delete_envelopes_dlq_messages_ttl}"
-
-    // region transformation URLs
-    TRANSFORMATION_URL_BULKSCAN = "${var.transformation_url_bulkscan}"
-    TRANSFORMATION_URL_PROBATE  = "${var.transformation_url_probate}"
-    UPDATE_URL_BULKSCAN = "${var.update_url_bulkscan}"
-    UPDATE_URL_PROBATE = "${var.update_url_probate}"
-    // endregion
-
-  }
-}
-
-module "bulk-scan-orchestrator" {
-  source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product                         = "${var.product}-${var.component}"
-  location                        = "${var.location_app}"
-  env                             = "${var.env}"
-  ilbIp                           = "${var.ilbIp}"
-  resource_group_name             = "${azurerm_resource_group.rg.name}"
-  subscription                    = "${var.subscription}"
-  capacity                        = "${var.capacity}"
-  common_tags                     = "${var.common_tags}"
-  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
-  asp_name                        = "${var.product}-${var.env}"
-  asp_rg                          = "${var.product}-${var.env}"
-  instance_size                   = "${local.sku_size}"
-  java_container_version          = "9.0"
-
-  app_settings                    = "${merge(local.core_app_settings, local.users_usernames_settings, local.users_passwords_settings)}"
-  enable_ase                      = "${var.enable_ase}"
 }
 
 data "azurerm_key_vault" "key_vault" {
--- infrastructure/prod.tfvars
+++
@@ -1,8 +0,0 @@
-capacity = "2"
-idam_api_url = "https://idam-api.platform.hmcts.net"
-idam_client_redirect_uri = "https://rpe-bulk-scan-processor-prod.service.core-compute-prod.internal/oauth2/callback"
-
-delete_envelopes_dlq_messages_enabled = "true"
-
-transformation_url_bulkscan = ""
-update_url_bulkscan = ""
