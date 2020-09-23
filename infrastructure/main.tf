5b2d1ee5c32c668844bbaff658839829d5cc9046
akomisarek <Adam.Komisarek@hmcts.net>
2020-09-23 14:55:54 +0100

    chore: update terraform to 0.13.3 and azurerm to 2.x (#1237)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
-  version = "=1.42.0"
+  features {}
 }
 
 # Make sure the resource group exists
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location_app}"
+  location = var.location_app
 }
 
 data "azurerm_key_vault" "key_vault" {
@@ -20,13 +20,13 @@ data "azurerm_key_vault" "s2s_key_vault" {
 
 
 data "azurerm_key_vault_secret" "s2s_secret" {
-  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.s2s_key_vault.id
   name      = "microservicekey-bulk-scan-orchestrator"
 }
 
 # Copy orchestrator s2s secret from s2s key vault to bulkscan key vault
 resource "azurerm_key_vault_secret" "bulk_scan_orchestrator_app_s2s_secret" {
   name         = "s2s-secret-bulk-scan-orchestrator"
-  value        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  value        = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
