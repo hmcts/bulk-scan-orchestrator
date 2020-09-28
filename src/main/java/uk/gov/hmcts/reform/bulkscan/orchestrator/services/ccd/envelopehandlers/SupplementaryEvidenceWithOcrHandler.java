aaf7ad8edeff9d68cd72e6b7fc51e1721121a9c6
Rafal Kondratowicz <rkondr@gmail.com>
2020-09-28 19:06:03 +0200

    Call auto case updater

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/envelopehandlers/SupplementaryEvidenceWithOcrHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/envelopehandlers/SupplementaryEvidenceWithOcrHandler.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.envelopehandlers;
 
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.PaymentsProcessor;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.autocaseupdate.AutoCaseUpdater;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.config.ServiceConfigProvider;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Classification;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.envelopes.model.Envelope;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.EnvelopeProcessingResult;
@@ -14,13 +16,19 @@ public class SupplementaryEvidenceWithOcrHandler {
 
     private final CreateExceptionRecord exceptionRecordCreator;
     private final PaymentsProcessor paymentsProcessor;
+    private final AutoCaseUpdater autoCaseUpdater;
+    private final ServiceConfigProvider serviceConfigProvider;
 
     public SupplementaryEvidenceWithOcrHandler(
         CreateExceptionRecord exceptionRecordCreator,
-        PaymentsProcessor paymentsProcessor
+        PaymentsProcessor paymentsProcessor,
+        AutoCaseUpdater autoCaseUpdater,
+        ServiceConfigProvider serviceConfigProvider
     ) {
         this.exceptionRecordCreator = exceptionRecordCreator;
         this.paymentsProcessor = paymentsProcessor;
+        this.autoCaseUpdater = autoCaseUpdater;
+        this.serviceConfigProvider = serviceConfigProvider;
     }
 
     public EnvelopeProcessingResult handle(Envelope envelope) {
@@ -29,9 +37,14 @@ public class SupplementaryEvidenceWithOcrHandler {
             "Envelope classification has to be " + Classification.SUPPLEMENTARY_EVIDENCE_WITH_OCR
         );
 
-        Long ccdId = exceptionRecordCreator.tryCreateFrom(envelope);
-        paymentsProcessor.createPayments(envelope, ccdId, true);
-
-        return new EnvelopeProcessingResult(ccdId, EXCEPTION_RECORD);
+        if (serviceConfigProvider.getConfig(envelope.container).getAutoCaseUpdateEnabled()) {
+            autoCaseUpdater.updateCase(envelope);
+            paymentsProcessor.createPayments(envelope, ccdId, false);
+            return new EnvelopeProcessingResult(ccdId, EXCEPTION_RECORD);
+        } else {
+            Long ccdId = exceptionRecordCreator.tryCreateFrom(envelope);
+            paymentsProcessor.createPayments(envelope, ccdId, true);
+            return new EnvelopeProcessingResult(ccdId, EXCEPTION_RECORD);
+        }
     }
 }
