a5d3ee8a8e09251e7cb658ffd7ff44ee94f2bda9
mdayican <mustafa.dayican@hmcts.net>
2020-10-19 15:19:55 +0100

    demo v2 infra changes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 data "azurerm_key_vault" "key_vault" {
-  name                = "${var.raw_product}-${var.env}"
-  resource_group_name = "${var.raw_product}-${var.env}"
+  name                = "${var.raw_product}-v2-${var.env}"
+  resource_group_name = "${var.raw_product}-v2-${var.env}"
 }
 
 data "azurerm_key_vault" "s2s_key_vault" {
