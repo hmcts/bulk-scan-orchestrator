cbb3df6fadc20983dcc2d324eddfb4b0db95d9bc
apereverzin <andrey.pereverzin@hotmail.com>
2020-11-19 10:55:46 +0000

    Added func test

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/AutomaticCaseUpdateTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/AutomaticCaseUpdateTest.java
@@ -13,6 +13,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -37,6 +38,41 @@ public class AutomaticCaseUpdateTest {
         //given
         String docUrl = dmUploadService.uploadToDmStore("Certificate.pdf", "documents/supplementary-evidence.pdf");
         CaseDetails existingCase = ccdCaseCreator.createCase(emptyList(), Instant.now());
+
+        // when
+        envelopeMessager.sendMessageFromFile(
+            "envelopes/supplementary-evidence-with-ocr-bulkscanauto.json",
+            String.valueOf(existingCase.getId()),
+            null,
+            randomUUID(),
+            docUrl
+        );
+
+        // then
+        await("Case in CCD was updated with data from envelope")
+            .atMost(60, TimeUnit.SECONDS)
+            .pollInterval(Duration.ofSeconds(2))
+            .until(() -> {
+                CaseDetails updatedCaseDetails = ccdApi.getCase(
+                    String.valueOf(existingCase.getId()),
+                    existingCase.getJurisdiction()
+                );
+                var address = (Map<String, String>) updatedCaseDetails.getData().get("address");
+                return address != null && Objects.equals(address.get("country"), "country-from-envelope");
+            });
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void should_update_a_case_with_ocr_values_set() throws Exception {
+        //given
+        String docUrl = dmUploadService.uploadToDmStore("Certificate.pdf", "documents/supplementary-evidence.pdf");
+
+        Map<String, Object> ocrFields = new HashMap<>();
+        // the values are from envelopes/supplementary-evidence-with-ocr-bulkscanauto.json file
+        ocrFields.put("firstName", "value1");
+        ocrFields.put("lastName", "value2");
+        CaseDetails existingCase = ccdCaseCreator.createCase(emptyList(), Instant.now(), ocrFields);
 
         // when
         envelopeMessager.sendMessageFromFile(
