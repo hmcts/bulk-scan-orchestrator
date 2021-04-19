2a406c627c6e0d90108645c8848f3168206b5b33
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2021-04-19 10:22:40 +0100

    Enable flyway (#1394)
    * enable-flyway
    
    * Latest version
    
    * Version upgrade
    
    Co-authored-by: Andrey Pereverzin <andrey.pereverzin@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/FlywayConfig.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.stream.Stream;
+
+@AutoConfigureAfter({DataSourceAutoConfiguration.class})
+@AutoConfigureBefore({FlywayAutoConfiguration.class})
+@Configuration
+@ConditionalOnClass(Flyway.class)
+@ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
+public class FlywayConfig {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "flyway", name = "skip-migrations", havingValue = "true")
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        // don't run migrations from the app, just check if all were applied
+        return flyway ->
+            Stream
+                .of(flyway.info().all())
+                .filter(migration -> !migration.getState().isApplied())
+                .findFirst()
+                .ifPresent(notAppliedMigration -> {
+                    throw new uk.gov.hmcts.reform.bulkscan.orchestrator.config.PendingMigrationException(
+                        "Pending migration: " + notAppliedMigration.getScript()
+                    );
+                });
+    }
+}
