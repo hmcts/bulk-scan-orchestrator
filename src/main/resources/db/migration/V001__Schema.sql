fcafa48792896bc338711af50e97277767a1727c
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2021-04-20 08:38:55 +0100

    DB Schema

--- src/main/resources/db/migration/V001__Schema.sql
+++ src/main/resources/db/migration/V001__Schema.sql
@@ -1,57 +1,29 @@
-CREATE TABLE envelopes (
+CREATE TABLE create_case_request (
   id UUID PRIMARY KEY,
-  processor_envelope_id UUID NOT NULL,
-  received_from_processor_at TIMESTAMP NOT NULL,
-  case_ref VARCHAR(50) NOT NULL,
-  legacy_case_ref VARCHAR(50) NOT NULL,
-  po_box VARCHAR(50) NOT NULL,
-  jurisdiction VARCHAR(50) NOT NULL,
-  container VARCHAR(50) NOT NULL,
-  zip_file_name VARCHAR(255) NOT NULL,
-  form_type VARCHAR(50) NOT NULL,
-  delivery_date TIMESTAMP NOT NULL,
-  opening_date TIMESTAMP NOT NULL,
-  classification VARCHAR(50) NOT NULL
+  created_at TIMESTAMP NOT NULL,
+  exception_record_id VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE documents (
+CREATE TABLE create_case_result (
   id UUID PRIMARY KEY,
-  processor_envelope_id UUID NOT NULL,
-  file_name VARCHAR(255) NOT NULL,
-  control_number VARCHAR(100),
-  type VARCHAR(50) NOT NULL,
-  sub_type VARCHAR(50) NOT NULL,
-  scanned_at TIMESTAMP,
-  uuid VARCHAR(50),
-  delivery_date TIMESTAMP
-);
-
-CREATE TABLE payments (
-  id UUID PRIMARY KEY,
-  processor_envelope_id UUID NOT NULL,
-  document_control_number VARCHAR(100),
-  envelope_id UUID REFERENCES envelopes(id)
-);
-
-CREATE TABLE ocr_data_fields (
-  id UUID PRIMARY KEY,
-  processor_envelope_id UUID NOT NULL,
-  name VARCHAR(50),
-  value VARCHAR(50),
-  envelope_id UUID REFERENCES envelopes(id)
-);
-
-CREATE TABLE ocr_data_validation_warnings (
-  id UUID PRIMARY KEY,
-  processor_envelope_id UUID NOT NULL,
-  text VARCHAR(255),
-  envelope_id UUID REFERENCES envelopes(id)
-);
-
-CREATE TABLE processed_envelopes (
-  id UUID PRIMARY KEY,
-  sent_to_processor_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL,
   ccd_id VARCHAR(50),
-  envelope_ccd_action VARCHAR(50),
-  envelope_id UUID REFERENCES envelopes(id)
+  errors VARCHAR(255),
+  warnings VARCHAR(255),
+  request_id UUID REFERENCES create_case_request(id)
+);
+
+CREATE TABLE attach_to_case_request (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  exception_record_id VARCHAR(50) NOT NULL,
+  target_case_ref VARCHAR(50)
+);
+
+CREATE TABLE attach_to_case_result (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  errors VARCHAR(255),
+  warnings VARCHAR(255),
+  request_id UUID REFERENCES attach_to_case_request(id)
 );
