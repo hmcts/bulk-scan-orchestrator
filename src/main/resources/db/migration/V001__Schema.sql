66fdc2d9a87f9cc285114e100f63265fc958c959
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2021-04-20 14:32:32 +0100

    Code review

--- src/main/resources/db/migration/V001__Schema.sql
+++ src/main/resources/db/migration/V001__Schema.sql
@@ -1,29 +1,7 @@
-CREATE TABLE create_case_request (
+CREATE TABLE callback_result (
   id UUID PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
+  request_type VARCHAR(50) NOT NULL
   exception_record_id VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE create_case_result (
-  id UUID PRIMARY KEY,
-  created_at TIMESTAMP NOT NULL,
-  ccd_id VARCHAR(50),
-  errors VARCHAR(255),
-  warnings VARCHAR(255),
-  request_id UUID REFERENCES create_case_request(id)
-);
-
-CREATE TABLE attach_to_case_request (
-  id UUID PRIMARY KEY,
-  created_at TIMESTAMP NOT NULL,
-  exception_record_id VARCHAR(50) NOT NULL,
-  target_case_ref VARCHAR(50)
-);
-
-CREATE TABLE attach_to_case_result (
-  id UUID PRIMARY KEY,
-  created_at TIMESTAMP NOT NULL,
-  errors VARCHAR(255),
-  warnings VARCHAR(255),
-  request_id UUID REFERENCES attach_to_case_request(id)
+  case_id VARCHAR(50)
 );
