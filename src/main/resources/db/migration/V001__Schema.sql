e6bc0873c95aa310249b5b8dae8d65b415502758
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2021-04-21 09:22:57 +0100

    SQL fixed (#1401)

--- src/main/resources/db/migration/V001__Schema.sql
+++ src/main/resources/db/migration/V001__Schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE callback_result (
   id UUID PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
-  request_type VARCHAR(50) NOT NULL
-  exception_record_id VARCHAR(50) NOT NULL
+  request_type VARCHAR(50) NOT NULL,
+  exception_record_id VARCHAR(50) NOT NULL,
   case_id VARCHAR(50)
 );
