396e8b3ba15d0ea40d8552ec2ee38140ae8212bd
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2021-04-23 11:07:01 +0100

    Repo layer (#1411)
    * CallbackResultRepository
    
    * code review
    
    * Repository depending on flyway
    
    * flyway task
    
    * Fixed db name
    
    
    Co-authored-by: Andrey Pereverzin <andrey.pereverzin@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/data/callbackresult/NewCallbackResult.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult;
+
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult.RequestType.ATTACH_TO_CASE;
+import static uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult.RequestType.CREATE_CASE;
+
+public class NewCallbackResult {
+    public final RequestType requestType;
+    public final String exceptionRecordId;
+    public final String caseId;
+
+    public static NewCallbackResult createCaseRequest(String exceptionRecordId, String caseId) {
+        return new NewCallbackResult(CREATE_CASE, exceptionRecordId, caseId);
+    }
+
+    public static NewCallbackResult attachToCaseCaseRequest(String exceptionRecordId, String caseId) {
+        return new NewCallbackResult(ATTACH_TO_CASE, exceptionRecordId, caseId);
+    }
+
+    private NewCallbackResult(
+        RequestType requestType,
+        String exceptionRecordId,
+        String caseId
+    ) {
+        this.requestType = requestType;
+        this.exceptionRecordId = exceptionRecordId;
+        this.caseId = caseId;
+    }
+}
