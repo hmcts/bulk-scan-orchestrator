d5b13f05b43856aa610860ceed7e921619ef5e37
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2021-04-28 12:51:14 +0100

    CallbackRepository find

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/data/CallbackResultRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/data/CallbackResultRepositoryTest.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.reform.bulkscan.orchestrator.config.IntegrationTest;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult.CallbackResultRepository;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult.NewCallbackResult;
 
+import java.time.Instant;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,7 +21,13 @@ import static uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult.Requ
 public class CallbackResultRepositoryTest {
 
     private static final String ER_ID_1 = "ER_ID_1";
+    private static final String ER_ID_2 = "ER_ID_2";
     private static final String CASE_ID_1 = "CASE_ID_1";
+    private static final String CASE_ID_2 = "CASE_ID_2";
+    private static final NewCallbackResult NEW_CALLBACK_RESULT_1 =
+        NewCallbackResult.attachToCaseCaseRequest(ER_ID_1, CASE_ID_1);
+    private static final NewCallbackResult NEW_CALLBACK_RESULT_2 =
+        NewCallbackResult.attachToCaseCaseRequest(ER_ID_2, CASE_ID_2);
 
     @Autowired private CallbackResultRepository repo;
     @Autowired private DbHelper dbHelper;
@@ -33,10 +40,10 @@ public class CallbackResultRepositoryTest {
     @Test
     void should_save_create_case_result() {
         // given
-        var newEnvelope = NewCallbackResult.createCaseRequest(ER_ID_1, CASE_ID_1);
+        var newCallbackResult = NewCallbackResult.createCaseRequest(ER_ID_1, CASE_ID_1);
 
         // when
-        UUID id = repo.insert(newEnvelope);
+        UUID id = repo.insert(newCallbackResult);
 
         // then
         dbHelper.getAllCallbackResults();
@@ -51,10 +58,10 @@ public class CallbackResultRepositoryTest {
     @Test
     void should_save_attach_to_case_result() {
         // given
-        var newEnvelope = NewCallbackResult.attachToCaseCaseRequest(ER_ID_1, CASE_ID_1);
+        var newCallbackResult = NewCallbackResult.attachToCaseCaseRequest(ER_ID_1, CASE_ID_1);
 
         // when
-        UUID id = repo.insert(newEnvelope);
+        UUID id = repo.insert(newCallbackResult);
 
         // then
         dbHelper.getAllCallbackResults();
@@ -64,5 +71,71 @@ public class CallbackResultRepositoryTest {
             .containsExactly(
                 tuple(id, ATTACH_TO_CASE, ER_ID_1, CASE_ID_1)
             );
+    }
+
+    @Test
+    void should_find_by_case_id() {
+        // given
+        var createdAt1 = Instant.now();
+        var createdAt2 = Instant.now();
+        var id1 = dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_1, createdAt1);
+        dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_2, createdAt2);
+
+        // when
+        var caseResultOpt = repo.findByCaseId(CASE_ID_1);
+
+        // then
+        assertThat(caseResultOpt.isEmpty()).isFalse();
+        assertThat(caseResultOpt.get())
+            .extracting(res -> tuple(res.id, res.createdAt, res.requestType, res.exceptionRecordId, res.caseId))
+            .isEqualTo(tuple(id1, createdAt1, ATTACH_TO_CASE, ER_ID_1, CASE_ID_1));
+    }
+
+    @Test
+    void should_find_by_exception_record_id() {
+        // given
+        var createdAt1 = Instant.now();
+        var createdAt2 = Instant.now();
+        var id1 = dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_1, createdAt1);
+        dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_2, createdAt2);
+
+        // when
+        var caseResultOpt = repo.findByExceptionRecordId(ER_ID_1);
+
+        // then
+        assertThat(caseResultOpt.isEmpty()).isFalse();
+        assertThat(caseResultOpt.get())
+            .extracting(res -> tuple(res.id, res.createdAt, res.requestType, res.exceptionRecordId, res.caseId))
+            .isEqualTo(tuple(id1, createdAt1, ATTACH_TO_CASE, ER_ID_1, CASE_ID_1));
+    }
+
+    @Test
+    void should_not_find_by_non_existing_case_id() {
+        // given
+        var createdAt1 = Instant.now();
+        var createdAt2 = Instant.now();
+        dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_1, createdAt1);
+        dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_2, createdAt2);
+
+        // when
+        var caseResultOpt = repo.findByCaseId("WRONG_CASE_ID");
+
+        // then
+        assertThat(caseResultOpt.isEmpty()).isTrue();
+    }
+
+    @Test
+    void should_not_find_by_non_existing_exception_record_id() {
+        // given
+        var createdAt1 = Instant.now();
+        var createdAt2 = Instant.now();
+        dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_1, createdAt1);
+        dbHelper.insertCallbackResultWithCreatedAt(NEW_CALLBACK_RESULT_2, createdAt2);
+
+        // when
+        var caseResultOpt = repo.findByExceptionRecordId("WRONG_ER_ID");
+
+        // then
+        assertThat(caseResultOpt.isEmpty()).isTrue();
     }
 }
--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/data/callbackresult/CallbackResultRepository.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/data/callbackresult/CallbackResultRepository.java
@@ -1,17 +1,23 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public class CallbackResultRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final CallbackResultRowMapper callbackResultRowMapper;
 
-    public CallbackResultRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+    public CallbackResultRepository(NamedParameterJdbcTemplate jdbcTemplate,
+                                    CallbackResultRowMapper callbackResultRowMapper
+    ) {
         this.jdbcTemplate = jdbcTemplate;
+        this.callbackResultRowMapper = callbackResultRowMapper;
     }
 
     public UUID insert(NewCallbackResult callbackResult) {
@@ -26,5 +32,31 @@ public class CallbackResultRepository {
                 .addValue("caseId", callbackResult.caseId)
         );
         return id;
+    }
+
+    public Optional<CallbackResult> findByExceptionRecordId(String exceptionRecordId) {
+        try {
+            CallbackResult callbackResult = jdbcTemplate.queryForObject(
+                "SELECT * FROM callback_result WHERE exception_record_id = :exceptionRecordId",
+                new MapSqlParameterSource("exceptionRecordId", exceptionRecordId),
+                callbackResultRowMapper
+            );
+            return Optional.of(callbackResult);
+        } catch (EmptyResultDataAccessException ex) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<CallbackResult> findByCaseId(String caseId) {
+        try {
+            CallbackResult callbackResult = jdbcTemplate.queryForObject(
+                "SELECT * FROM callback_result WHERE case_id = :caseId",
+                new MapSqlParameterSource("caseId", caseId),
+                callbackResultRowMapper
+            );
+            return Optional.of(callbackResult);
+        } catch (EmptyResultDataAccessException ex) {
+            return Optional.empty();
+        }
     }
 }
