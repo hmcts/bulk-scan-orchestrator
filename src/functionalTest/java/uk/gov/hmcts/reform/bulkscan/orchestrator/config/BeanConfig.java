3b1cba13503faf6e8bb3fdc136a7abb0e8cd1a2b
mdayican <mustafa.dayican@hmcts.net>
2021-04-28 18:25:55 +0100

    use AutoConfigureTestDatabase

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/BeanConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/BeanConfig.java
@@ -1,17 +1,19 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.data.callbackresult.CallbackResultRepository;
 
 import static org.mockito.Mockito.mock;
 
-@TestConfiguration
+@Configuration
+@AutoConfigureTestDatabase
 public class BeanConfig {
 
-    @Bean("callbackResultRepository")
+    @Bean
     @Primary
     public CallbackResultRepository getCallbackResultRepository() {
         return new CallbackResultRepository(mock(NamedParameterJdbcTemplate.class));
