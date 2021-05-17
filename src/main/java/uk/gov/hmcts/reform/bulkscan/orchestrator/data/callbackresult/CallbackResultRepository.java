fde92ea6be4abb382523a40cf6cc727adaa4f911
mdayican <mustafa.dayican@hmcts.net>
2021-05-17 09:10:32 +0100

    Revert "Revert "CallbackRepository find methods (#1417)""
    This reverts commit 6414576bddcd340537b0011b08b58bdb985cd043.

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/data/callbackresult/CallbackResultRepository.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/data/callbackresult/CallbackResultRepository.java
@@ -4,14 +4,19 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public class CallbackResultRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final CallbackResultRowMapper callbackResultRowMapper;
 
-    public CallbackResultRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+    public CallbackResultRepository(NamedParameterJdbcTemplate jdbcTemplate,
+                                    CallbackResultRowMapper callbackResultRowMapper
+    ) {
         this.jdbcTemplate = jdbcTemplate;
+        this.callbackResultRowMapper = callbackResultRowMapper;
     }
 
     public UUID insert(NewCallbackResult callbackResult) {
@@ -26,5 +31,21 @@ public class CallbackResultRepository {
                 .addValue("caseId", callbackResult.caseId)
         );
         return id;
+    }
+
+    public List<CallbackResult> findByExceptionRecordId(String exceptionRecordId) {
+        return jdbcTemplate.query(
+            "SELECT * FROM callback_result WHERE exception_record_id = :exceptionRecordId",
+            new MapSqlParameterSource("exceptionRecordId", exceptionRecordId),
+            callbackResultRowMapper
+        );
+    }
+
+    public List<CallbackResult> findByCaseId(String caseId) {
+        return jdbcTemplate.query(
+            "SELECT * FROM callback_result WHERE case_id = :caseId",
+            new MapSqlParameterSource("caseId", caseId),
+            callbackResultRowMapper
+        );
     }
 }
