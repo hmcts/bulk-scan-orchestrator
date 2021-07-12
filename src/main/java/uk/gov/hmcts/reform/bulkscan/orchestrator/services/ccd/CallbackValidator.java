b305909b0ae627f2748b3e98b3dd428fdc8c84a3
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2021-07-12 14:54:52 +0100

    Moved hasFormType to CallbackValidator (#1467)
    Co-authored-by: Andrey Pereverzin <andrey.pereverzin@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackValidator.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/CallbackValidator.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd;
 
 import io.vavr.control.Validation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
@@ -11,11 +13,22 @@ import static io.vavr.control.Validation.invalid;
 
 @Component
 public class CallbackValidator {
+    private static final Logger log = LoggerFactory.getLogger(CallbackValidator.class);
+
     @Nonnull
     public Validation<String, String> hasCaseTypeId(CaseDetails theCase) {
         return Optional.ofNullable(theCase)
                 .map(CaseDetails::getCaseTypeId)
                 .map(Validation::<String, String>valid)
                 .orElse(invalid("Missing caseType"));
+    }
+
+    @Nonnull
+    public Validation<String, String> hasFormType(CaseDetails theCase) {
+        return Optional.ofNullable(theCase)
+                .map(CaseDetails::getData)
+                .map(data -> (String) data.get("formType"))
+                .map(Validation::<String, String>valid)
+                .orElse(invalid("Missing Form Type"));
     }
 }
