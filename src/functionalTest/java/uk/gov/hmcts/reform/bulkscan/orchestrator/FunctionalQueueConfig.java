7546733af5d0045367ab1d6a8808ca5b44cba978
MUSTAFA <mustafa.dayican@hmcts.net>
2021-07-12 18:46:43 +0100

    DTSBPS-662 remove old version receiver form func test (#1469)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/FunctionalQueueConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/FunctionalQueueConfig.java
@@ -1,23 +1,17 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator;
 
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusReceiverClient;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
-import com.microsoft.azure.servicebus.ClientFactory;
-import com.microsoft.azure.servicebus.IMessageReceiver;
-import com.microsoft.azure.servicebus.ReceiveMode;
-import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
-import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import org.apache.commons.lang3.StringUtils;
+import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
+import com.azure.messaging.servicebus.models.SubQueue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.IPaymentsPublisher;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.processedenvelopes.IProcessedEnvelopeNotifier;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.exceptions.ConnectionException;
 
 import java.util.function.Supplier;
-
-import static org.mockito.Mockito.mock;
 
 public class FunctionalQueueConfig {
     public static final String CONNECTION_STR_FORMAT =
@@ -44,41 +38,22 @@ public class FunctionalQueueConfig {
     @Bean
     public ServiceBusSenderClient testWriteClient() {
         return new ServiceBusClientBuilder()
-            .connectionString(
-                String.format(
-                    CONNECTION_STR_FORMAT,
-                    queueNamespace,
-                    queueWriteAccessKeyName,
-                    queueWriteAccessKey
-                )
-            )
+            .connectionString(getEnvelopQueueConnectionString())
             .sender()
             .queueName(queueName)
             .buildClient();
     }
 
-
     @Bean(name = "dlqReceiver")
-    public Supplier<IMessageReceiver> dlqReceiverProvider() {
-        return () -> {
-            try {
-                return ClientFactory.createMessageReceiverFromConnectionStringBuilder(
-                    new ConnectionStringBuilder(
-                        queueNamespace,
-                        StringUtils.join(queueName, "/$deadletterqueue"),
-                        queueReadAccessKeyName,
-                        queueReadAccessKey
-                    ),
-                    ReceiveMode.PEEKLOCK
-                );
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new ConnectionException("Unable to connect to the dlq", e);
-            } catch (ServiceBusException e) {
-                throw new ConnectionException("Unable to connect to the dlq", e);
-            }
-        };
-
+    public Supplier<ServiceBusReceiverClient> dlqReceiverProvider() {
+        return () ->
+            new ServiceBusClientBuilder()
+                .connectionString(getEnvelopQueueConnectionString())
+                .receiver()
+                .queueName(queueName)
+                .receiveMode(ServiceBusReceiveMode.PEEK_LOCK)
+                .subQueue(SubQueue.DEAD_LETTER_QUEUE)
+                .buildClient();
     }
 
     @Bean
@@ -97,9 +72,13 @@ public class FunctionalQueueConfig {
         };
     }
 
-    @Bean
-    @Profile("nosb") // apply only when Service Bus should not be used
-    public IMessageReceiver testMessageReceiver() {
-        return mock(IMessageReceiver.class);
+    private String getEnvelopQueueConnectionString() {
+        return String.format(
+            CONNECTION_STR_FORMAT,
+            queueNamespace,
+            queueWriteAccessKeyName,
+            queueWriteAccessKey
+        );
     }
+
 }
