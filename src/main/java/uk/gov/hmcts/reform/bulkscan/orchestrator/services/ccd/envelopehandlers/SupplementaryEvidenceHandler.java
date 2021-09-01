1e77f789baab4293f3a8cc552cfd7f14570d9c23
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2021-09-01 17:24:31 +0100

    Added log

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/envelopehandlers/SupplementaryEvidenceHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/envelopehandlers/SupplementaryEvidenceHandler.java
@@ -64,7 +64,11 @@ public class SupplementaryEvidenceHandler {
                 return new EnvelopeProcessingResult(erId, EXCEPTION_RECORD);
             }
         } else {
-            log.info("Case not found. Creating exception record instead");
+            log.info(
+                    "Case not found, caseRef {}, legacyCaseRef {}. Creating exception record instead",
+                    envelope.caseRef,
+                    envelope.legacyCaseRef
+            );
             Long erId = exceptionRecordCreator.tryCreateFrom(envelope);
             paymentsProcessor.createPayments(envelope, erId, true);
             return new EnvelopeProcessingResult(erId, EXCEPTION_RECORD);
