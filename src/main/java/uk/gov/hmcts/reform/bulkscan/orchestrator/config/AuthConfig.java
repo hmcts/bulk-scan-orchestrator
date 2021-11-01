ca01a1040751a2a1af1643d0914e13f3ac446676
mdayican <mustafa.dayican@hmcts.net>
2021-11-01 13:26:06 +0000

    DTSBPS-743 make authconfig authTokenGenerator primary

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AuthConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AuthConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
@@ -14,6 +15,7 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 public class AuthConfig {
 
     @Bean
+    @Primary
     public AuthTokenGenerator authTokenGenerator(
         @Value("${idam.s2s-auth.secret}") String secret,
         @Value("${idam.s2s-auth.name}") String name,
