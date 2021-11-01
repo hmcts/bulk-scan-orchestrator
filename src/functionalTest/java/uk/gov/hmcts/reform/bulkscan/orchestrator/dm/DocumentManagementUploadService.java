4fcb4fee455fb61d900abfa625d56a8b94236c00
MUSTAFA <mustafa.dayican@hmcts.net>
2021-11-01 16:39:03 +0000

    DTSBPS-743 use processor s2s to upload for functional test  (#1625)

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/dm/DocumentManagementUploadService.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/dm/DocumentManagementUploadService.java
@@ -2,12 +2,14 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.dm;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.SampleData;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdAuthenticator;
-import uk.gov.hmcts.reform.bulkscan.orchestrator.services.ccd.CcdAuthenticatorFactory;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.cache.CachedIdamCredential;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.idam.cache.IdamCachedClient;
 import uk.gov.hmcts.reform.document.DocumentUploadClientApi;
 import uk.gov.hmcts.reform.document.domain.UploadResponse;
 import uk.gov.hmcts.reform.document.utils.InMemoryMultipartFile;
@@ -21,16 +23,20 @@ public class DocumentManagementUploadService {
 
     private final DocumentUploadClientApi documentUploadClientApi;
 
-    private final CcdAuthenticatorFactory ccdAuthenticatorFactory;
+    private final AuthTokenGenerator s2sTokenGenerator;
+    private final IdamCachedClient idamCachedClient;
 
     private static final String FILES_NAME = "files";
 
     DocumentManagementUploadService(
-        CcdAuthenticatorFactory ccdAuthenticatorFactory,
-        DocumentUploadClientApi documentUploadClientApi
+        @Qualifier("processor-s2s-auth") AuthTokenGenerator s2sTokenGenerator,
+        DocumentUploadClientApi documentUploadClientApi,
+        IdamCachedClient idamCachedClient
     ) {
-        this.ccdAuthenticatorFactory = ccdAuthenticatorFactory;
+        this.s2sTokenGenerator = s2sTokenGenerator;
         this.documentUploadClientApi = documentUploadClientApi;
+        this.idamCachedClient = idamCachedClient;
+
     }
 
     /**
@@ -47,12 +53,13 @@ public class DocumentManagementUploadService {
             SampleData.fileContentAsBytes(filePath)
         );
 
-        CcdAuthenticator authenticator = ccdAuthenticatorFactory.createForJurisdiction(SampleData.JURSIDICTION);
+        CachedIdamCredential idamCredentials = idamCachedClient.getIdamCredentials(SampleData.JURSIDICTION);
+        String s2sToken = s2sTokenGenerator.generate();
 
         UploadResponse uploadResponse = documentUploadClientApi.upload(
-            authenticator.getUserToken(),
-            authenticator.getServiceToken(),
-            authenticator.getUserId(),
+            idamCredentials.accessToken,
+            s2sToken,
+            idamCredentials.userId,
             singletonList(file)
         );
         log.info("{} uploaded to DM store", displayName);
--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/dm/ProcessorAuthConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/dm/ProcessorAuthConfig.java
@@ -1,24 +1,19 @@
-package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
+package uk.gov.hmcts.reform.bulkscan.orchestrator.dm;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
-import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @Configuration
-@EnableFeignClients(clients = IdamApi.class)
-public class AuthConfig {
+public class ProcessorAuthConfig {
 
-    @Bean
-    @Primary
+    @Bean("processor-s2s-auth")
     public AuthTokenGenerator authTokenGenerator(
-        @Value("${idam.s2s-auth.secret}") String secret,
-        @Value("${idam.s2s-auth.name}") String name,
+        @Value("${idam.s2s-auth.processor.secret}") String secret,
+        @Value("${idam.s2s-auth.processor.name}") String name,
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, name, serviceAuthorisationApi);
