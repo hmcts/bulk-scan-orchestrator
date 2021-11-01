2ddf2d18e2cb2bad4efa0c82f41894299b957a0e
mdayican <mustafa.dayican@hmcts.net>
2021-11-01 12:58:23 +0000

    DTSBPS-743 remove idaapi feign setting

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/dm/ProcessorAuthConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/dm/ProcessorAuthConfig.java
@@ -1,16 +1,13 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.dm;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
-import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @Configuration
-@EnableFeignClients(clients = IdamApi.class)
 public class ProcessorAuthConfig {
 
     @Bean("processor-s2s-auth")
