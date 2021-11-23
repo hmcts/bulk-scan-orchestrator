6f93a4de95279b5abe25f957ac64c1c22b9cd54b
mdayican <mustafa.dayican@hmcts.net>
2021-11-23 22:40:18 +0000

    DTSBPS-743 add extra logs controller

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcdCallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/CcdCallbackController.java
@@ -50,7 +50,7 @@ public class CcdCallbackController {
         @RequestHeader(value = "Authorization", required = false) String idamToken,
         @RequestHeader(value = USER_ID, required = false) String userId
     ) {
-        log.info("callback  id {}", callback.getEventId());
+        log.info("callback  ----");
 
         if (callback != null && callback.getCaseDetails() != null) {
 
