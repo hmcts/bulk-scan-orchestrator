fdf837774560e53b9f9d2afe9a2efb88bbb7a04e
amstevenson <addstevenson@hotmail.com>
2023-03-14 16:59:34 +0000

    update to make more robust

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -11,17 +11,20 @@ ENV="${3}"
 sudo ./create-env-file.sh "${KEY_VAULT}" "${SERVICE_NAME}" "${ENV}"
 
 while true; do
-    read -p "Do you wish to run this in docker? Yy or Nn: " yn
+    read -r -p "Do you wish to run both the service and db in docker (N will only up the db - if there is one) Yy or Nn: " yn
     case $yn in
         [Yy]* )
+          sed -i '' 's/localhost/host.docker.internal/' ../.localenv
           docker-compose down -v
           docker-compose build
           docker-compose up -d
-          sed 's/localhost/host.docker.internal/' substitutions.json
           echo "Setup complete! You can manage these services now via normal docker commands. Double check all is well with docker-compose ps"
           break;;
         [Nn]* )
-          sed 's/host.docker.internal/localhost/' substitutions.json
+          sed -i '' 's/host.docker.internal/localhost/' ../.localenv
+          docker-compose down -v
+          docker-compose build "${SERVICE_NAME}-db"
+          docker-compose up -d "${SERVICE_NAME}-db"
           echo "Setup complete! Next step is to add the .localenv file through the ENV plugin and run the application afterwards"
           exit;;
     esac
