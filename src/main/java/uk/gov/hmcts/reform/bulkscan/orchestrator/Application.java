0a200487009e5aaa2f24edbe2315afbf9f870d3b
majid.najib <majid.najib@justice.gov.uk>
2024-03-19 17:50:06 +0000

    update

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/Application.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/Application.java
@@ -1,17 +1,13 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform"})
-@ImportAutoConfiguration({FeignAutoConfiguration.class, HttpClientConfiguration.class})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
