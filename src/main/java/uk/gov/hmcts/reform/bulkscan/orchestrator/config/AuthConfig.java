f7586c4f0e34d280570a352b746b2b79517367ca
majid.najib <majid.najib@justice.gov.uk>
2024-03-25 16:47:26 +0000

    update

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AuthConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/AuthConfig.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +8,6 @@ import org.springframework.context.annotation.Lazy;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
-import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
-import uk.gov.hmcts.reform.authorisation.validators.ServiceAuthTokenValidator;
-
-import java.util.List;
 
 
 @Configuration
@@ -27,40 +22,5 @@ public class AuthConfig {
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, name, serviceAuthorisationApi);
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "idam.s2s-auth.url", havingValue = "false")
-    public AuthTokenGenerator authTokenGeneratorStub() {
-        return () -> {
-            throw new NotImplementedException();
-        };
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "idam.s2s-auth.url")
-    public AuthTokenValidator tokenValidator(ServiceAuthorisationApi s2sApi) {
-        return new ServiceAuthTokenValidator(s2sApi);
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "idam.s2s-auth.url", havingValue = "false")
-    public AuthTokenValidator tokenValidatorStub() {
-        return new AuthTokenValidator() {
-            @Override
-            public void validate(String token) {
-                throw new NotImplementedException();
-            }
-
-            @Override
-            public void validate(String token, List<String> roles) {
-                throw new NotImplementedException();
-            }
-
-            @Override
-            public String getServiceName(String token) {
-                return "some_service_name";
-            }
-        };
     }
 }
