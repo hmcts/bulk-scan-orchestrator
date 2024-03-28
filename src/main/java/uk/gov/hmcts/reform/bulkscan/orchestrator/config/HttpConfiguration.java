4dfe5a9f1ae127ab3f5fce951693520a0b8845f5
majid.najib <majid.najib@justice.gov.uk>
2024-03-28 14:10:42 +0000

    clean up

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/HttpConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/HttpConfiguration.java
@@ -1,16 +1,11 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
 import feign.Logger;
-import org.apache.hc.core5.http.Header;
-import org.apache.hc.core5.http.message.BasicHeader;
 import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collection;
-import java.util.Collections;
 
 @Configuration
 public class HttpConfiguration {
@@ -38,15 +33,9 @@ public class HttpConfiguration {
                 .setConnectionRequestTimeout(Timeout.ofSeconds(30))
                 .build();
 
-        Header header = new
-            BasicHeader("user-agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
-                + "like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        Collection<Header> defaultHeaders = Collections.singletonList(header);
         return org.apache.hc.client5.http.impl.classic.HttpClientBuilder
             .create()
             .useSystemProperties()
-            .setDefaultHeaders(defaultHeaders)
             .setDefaultRequestConfig(config)
             .build();
     }
