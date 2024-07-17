590c74a13a9eed66cc276c989b7c43500384ba73
Josh <joshblackmoor@users.noreply.github.com>
2024-07-17 10:43:14 +0100

    FACT-1837 remove unused stg db (#2658)
    * Remove stg db
    
    * Updating Terraform Formatting
    
    ---------
    
    Co-authored-by: 58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- infrastructure/tf-db-flexible.tf
+++ infrastructure/tf-db-flexible.tf
@@ -32,28 +32,3 @@ module "postgresql" {
   force_schema_ownership_trigger = "true"
   force_user_permissions_trigger = "1"
 }
-
-module "postgresql-staging" {
-  count = var.env == "aat" ? 1 : 0
-  providers = {
-    azurerm.postgres_network = azurerm.postgres_network
-  }
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
-  name                 = "${var.product}-${var.component}-flexible-v15-staging"
-  product              = var.product
-  component            = var.component
-  location             = var.location_db
-  env                  = "aat"
-  pgsql_admin_username = local.db_name
-  pgsql_databases = [
-    {
-      name : local.db_name
-    }
-  ]
-  common_tags   = var.common_tags
-  business_area = "cft"
-  pgsql_version = "15"
-  subnet_suffix = "expanded"
-
-  admin_user_object_id = var.jenkins_AAD_objectId
-}
