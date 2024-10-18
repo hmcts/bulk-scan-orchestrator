d45ced8ada52d46fb65522219e338ccb89294fa4
Imran Ali <Imran.Ali@justice.gov.uk>
2024-10-18 13:46:07 +0100

    remove line

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/JacksonConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/config/JacksonConfig.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
