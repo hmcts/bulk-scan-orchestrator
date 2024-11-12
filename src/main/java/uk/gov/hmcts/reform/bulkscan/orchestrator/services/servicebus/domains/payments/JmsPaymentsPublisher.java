040357075e2d8b58b2eb8aeb847e1ba8e226fac3
majid.najib <majid.najib@justice.gov.uk>
2024-11-12 14:02:22 +0000

    remove call to payment queue

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/domains/payments/JmsPaymentsPublisher.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/domains/payments/JmsPaymentsPublisher.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.jms.JMSException;
-import jakarta.jms.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.model.PaymentCommand;
 
@@ -36,34 +33,34 @@ public class JmsPaymentsPublisher implements IPaymentsPublisher {
 
     @Override
     public void send(PaymentCommand cmd) {
-        try {
-
-            String messageContent = objectMapper.writeValueAsString(cmd);
-            String messageId = UUID.randomUUID().toString();
-
-            LOG.info("About to send message to payments queue. ID: {}, Content: {}",
-                    messageId,
-                    messageContent
-            );
-
-            jmsTemplate.convertAndSend("payments", messageContent, new MessagePostProcessor() {
-                @Override
-                public Message postProcessMessage(Message message) throws JMSException {
-                    message.setJMSMessageID(messageId);
-                    return message;
-                }
-            });
-
-            LOG.info(
-                "Sent message to payments queue. ID: {}, Content: {}",
-                messageId,
-                messageContent
-            );
-        } catch (Exception ex) {
-            throw new PaymentsPublishingException(
-                "An error occurred when trying to publish message to payments queue.",
-                ex
-            );
-        }
+    //        try {
+    //
+    //            String messageContent = objectMapper.writeValueAsString(cmd);
+    //            String messageId = UUID.randomUUID().toString();
+    //
+    //            LOG.info("About to send message to payments queue. ID: {}, Content: {}",
+    //                    messageId,
+    //                    messageContent
+    //            );
+    //
+    //            jmsTemplate.convertAndSend("payments", messageContent, new MessagePostProcessor() {
+    //                @Override
+    //                public Message postProcessMessage(Message message) throws JMSException {
+    //                    message.setJMSMessageID(messageId);
+    //                    return message;
+    //                }
+    //            });
+    //
+    //            LOG.info(
+    //                "Sent message to payments queue. ID: {}, Content: {}",
+    //                messageId,
+    //                messageContent
+    //            );
+    //        } catch (Exception ex) {
+    //            throw new PaymentsPublishingException(
+    //                "An error occurred when trying to publish message to payments queue.",
+    //                ex
+    //            );
+    //        }
     }
 }
