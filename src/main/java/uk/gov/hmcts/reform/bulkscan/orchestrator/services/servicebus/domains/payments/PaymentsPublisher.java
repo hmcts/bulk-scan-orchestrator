3b81165c56ded1e2128371a26e1b789444361d25
majid.najib <majid.najib@justice.gov.uk>
2024-11-12 11:51:58 +0000

    remove call to payment queue

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/domains/payments/PaymentsPublisher.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/servicebus/domains/payments/PaymentsPublisher.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments;
 
-import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -12,9 +11,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.model.PaymentCommand;
 
-import java.util.UUID;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Service
 @Profile("!nosb") // do not register for the nosb (test) profile
@@ -44,55 +40,55 @@ public class PaymentsPublisher implements IPaymentsPublisher {
 
     @Override
     public void send(PaymentCommand cmd) {
-//        try {
-//            final String messageContent = objectMapper.writeValueAsString(cmd);
-//
-//            ServiceBusMessage message = new ServiceBusMessage(messageContent);
-//            message.setContentType(APPLICATION_JSON.toString());
-//            message.setMessageId(UUID.randomUUID().toString());
-//
-//            message.setSubject(cmd.getLabel());
-//
-//            LOG.info("About to send message to payments queue. ID: {}, Label: {}, Content: {}",
-//                    message.getMessageId(),
-//                    message.getSubject(),
-//                    messageContent
-//            );
-//
-//            doSend(message, retryCount);
-//
-//            LOG.info(
-//                "Sent message to payments queue. ID: {}, Label: {}, Content: {}",
-//                message.getMessageId(),
-//                message.getSubject(),
-//                messageContent
-//            );
-//        } catch (Exception ex) {
-//            throw new PaymentsPublishingException(
-//                "An error occurred when trying to publish message to payments queue.",
-//                ex
-//            );
-//        }
+    //        try {
+    //            final String messageContent = objectMapper.writeValueAsString(cmd);
+    //
+    //            ServiceBusMessage message = new ServiceBusMessage(messageContent);
+    //            message.setContentType(APPLICATION_JSON.toString());
+    //            message.setMessageId(UUID.randomUUID().toString());
+    //
+    //            message.setSubject(cmd.getLabel());
+    //
+    //            LOG.info("About to send message to payments queue. ID: {}, Label: {}, Content: {}",
+    //                    message.getMessageId(),
+    //                    message.getSubject(),
+    //                    messageContent
+    //            );
+    //
+    //            doSend(message, retryCount);
+    //
+    //            LOG.info(
+    //                "Sent message to payments queue. ID: {}, Label: {}, Content: {}",
+    //                message.getMessageId(),
+    //                message.getSubject(),
+    //                messageContent
+    //            );
+    //        } catch (Exception ex) {
+    //            throw new PaymentsPublishingException(
+    //                "An error occurred when trying to publish message to payments queue.",
+    //                ex
+    //            );
+    //        }
     }
 
-//    private void doSend(ServiceBusMessage message, int retryCount) throws InterruptedException {
-//        try {
-//            queueClient.sendMessage(message);
-//        } catch (Exception ex) {
-//            if (retryCount > 0) {
-//                LOG.error(
-//                        "Sent message to payments queue got error, "
-//                                + "Message ID: {}. Remaining Retry Count: {}, Retrying...",
-//                        message.getMessageId(),
-//                        retryCount,
-//                        ex
-//                );
-//                Thread.sleep(retryWait);
-//                doSend(message, --retryCount);
-//            } else {
-//                throw ex;
-//            }
-//
-//        }
-//    }
+    //    private void doSend(ServiceBusMessage message, int retryCount) throws InterruptedException {
+    //        try {
+    //            queueClient.sendMessage(message);
+    //        } catch (Exception ex) {
+    //            if (retryCount > 0) {
+    //                LOG.error(
+    //                        "Sent message to payments queue got error, "
+    //                                + "Message ID: {}. Remaining Retry Count: {}, Retrying...",
+    //                        message.getMessageId(),
+    //                        retryCount,
+    //                        ex
+    //                );
+    //                Thread.sleep(retryWait);
+    //                doSend(message, --retryCount);
+    //            } else {
+    //                throw ex;
+    //            }
+    //
+    //        }
+    //    }
 }
