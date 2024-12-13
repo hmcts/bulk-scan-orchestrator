b6aa65a98d47105189c928197f6f4e00d223380a
majid.najib <majid.najib@justice.gov.uk>
2024-12-13 11:31:50 +0000

    update

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/entity/PaymentRepository.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/entity/PaymentRepository.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,5 +10,9 @@ import java.util.UUID;
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 
     List<Payment> getPaymentsByStatus(String status);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Payment SET status = :status WHERE envelopeId = :envelopeId")
+    void updateStatusByEnvelopeId(String status, String envelopeId);
 
 }
