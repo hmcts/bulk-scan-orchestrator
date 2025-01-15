927f872549cac6dcc7c98c91050e187b145e0f94
majid.najib <majid.najib@justice.gov.uk>
2025-01-15 09:52:35 +0000

    update

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/AttachExceptionRecordToExistingCaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/services/ccd/AttachExceptionRecordToExistingCaseTest.java
@@ -5,8 +5,11 @@ import com.google.common.collect.ImmutableList;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.config.IntegrationTest;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.payment.PaymentService;
+import uk.gov.hmcts.reform.bulkscan.orchestrator.services.servicebus.domains.payments.PaymentsPublishingException;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
@@ -19,6 +22,8 @@ import static com.github.tomakehurst.wiremock.client.WireMock.status;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willThrow;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.config.Environment.CASE_REF;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.config.Environment.CASE_TYPE_EXCEPTION_RECORD;
 import static uk.gov.hmcts.reform.bulkscan.orchestrator.config.Environment.JURISDICTION;
@@ -32,6 +37,8 @@ class AttachExceptionRecordToExistingCaseTest extends AttachExceptionRecordTestB
     private static final String CASE_REFERENCE_TYPE_EXTERNAL = "externalCaseReference";
     private static final String CASE_REFERENCE_TYPE_CCD = "ccdCaseReference";
 
+    @Autowired
+    private PaymentService paymentService;
 
     @DisplayName("Should successfully callback with correct information")
     @Test
@@ -544,6 +551,10 @@ class AttachExceptionRecordToExistingCaseTest extends AttachExceptionRecordTestB
     @Test
     void should_fail_with_the_correct_error_when_payments_fails() {
         CallbackRequest callbackRequest = exceptionRecordCallbackRequestWithPayment();
+
+        willThrow(new PaymentsPublishingException("Payment failed", new RuntimeException("connection")))
+            .given(paymentService).savePayment(any());
+
         given()
             .body(callbackRequest)
             .headers(userHeaders())
