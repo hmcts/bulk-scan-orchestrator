e3b6488bbda7937c27ade02712c42cf5f5dc323d
Imran Ali <Imran.Ali@justice.gov.uk>
2025-01-30 11:31:35 +0000

    added exception for payment processing

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/tasks/payment/PaymentProcessingTask.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/tasks/payment/PaymentProcessingTask.java
@@ -57,9 +57,10 @@ public class PaymentProcessingTask {
                     }
                 });
             }
-        } catch (Exception e) {
-            log.debug("Error posting payments to payment api client. {}", e.getMessage());
-        }
+            } catch (Exception e) {
+                log.error("Error posting payments to payment API client", e);
+                throw new PaymentProcessingException("Failed to post payments to payment API client", e);
+            }
     }
 
     private ResponseEntity<String> postPaymentsToPaymentApi(Payment payment, int retryCount) {
