0ad1264ab0301b257fc5742a6885dacd4968826a
Ruth Bovell <ruthb111@outlook.com>
2025-04-01 18:51:32 +0100

    - fix test now auth is required

--- src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/SendReportControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/orchestrator/controllers/SendReportControllerTest.java
@@ -40,7 +40,7 @@ public class SendReportControllerTest {
     @Test
     public void should_send_daily_report() throws Exception {
         try (MockedStatic<Util> mockedStatic = mockStatic(Util.class)) {
-            this.mockMvc.perform(post("/reports/email-daily-report"))
+            this.mockMvc.perform(post("/reports/email-daily-report").header(HttpHeaders.AUTHORIZATION, "Bearer valid"))
                 .andDo(print()).andExpect(status().isOk());
             verify(sendPaymentReportService, times(1)).send();
         }
@@ -49,7 +49,7 @@ public class SendReportControllerTest {
     @Test
     public void should_send_report_by_date() throws Exception {
         try (MockedStatic<Util> mockedStatic = mockStatic(Util.class)) {
-            this.mockMvc.perform(post("/reports/email-report?date=2025-04-03"))
+            this.mockMvc.perform(post("/reports/email-report?date=2025-04-03").header(HttpHeaders.AUTHORIZATION, "Bearer valid"))
                 .andDo(print()).andExpect(status().isOk());
             verify(sendPaymentReportService, times(1)).send(LocalDate.of(2025, 4, 3));
         }
