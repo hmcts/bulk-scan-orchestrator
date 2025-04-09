edf2f5b29436273be3ddc38e931894d2f781ad82
Josh Blackmoor <joshua.blackmoor@justice.gov.uk>
2025-04-09 17:24:34 +0100

    Remove unused import and update DTO

--- src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/model/PaymentInfoDTO.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/orchestrator/model/PaymentInfoDTO.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.bulkscan.orchestrator.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class PaymentInfoDTO {
+
+    @JsonProperty(value = "document_control_number", required = true)
     private String documentControlNumber;
 }
