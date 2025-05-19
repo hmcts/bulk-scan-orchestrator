1f943c4771bc6988c58a4c1bf8672584745675df
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-05-19 15:16:47 +0100

    Fix int tests

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/tasks/EnvelopesQueueConsumeTaskTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/orchestrator/tasks/EnvelopesQueueConsumeTaskTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.bulkscan.orchestrator.tasks;
 
 import com.azure.messaging.servicebus.ServiceBusProcessorClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import uk.gov.hmcts.reform.bulkscan.orchestrator.config.IntegrationTest;
@@ -9,6 +10,7 @@ import uk.gov.hmcts.reform.bulkscan.orchestrator.config.IntegrationTest;
 import static org.mockito.Mockito.verify;
 
 @IntegrationTest
+@SpringBootTest(properties = {"scheduling.task.check-envelopes-queue.enabled=true"})
 public class EnvelopesQueueConsumeTaskTest {
 
     @MockitoSpyBean
